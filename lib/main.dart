639fc7f59523c7cf8ffb4bdee49f9d230cd4d238
MinJaehyun <krism01@naver.com>
2024-04-19 23:36:25 +0900

    drawer widget 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:simple_note/view/HomePageBodyFrame.dart';
+import 'package:simple_note/view/drawer_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,140 +26,35 @@ class MyPage extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
-        // note: 하단 footer 설정
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {
-            // Add your onPressed code here!
-          },
-          label: const Text('Add'),
-          icon: const Icon(Icons.add),
-        ),
+        // note: Appbar
+        appBar: buildAppBar(),
+        // note: home page body frame
+        body: HomePageBodyFrame(categories: categories),
+        // note: 하단 add
+        floatingActionButton: buildFloatingActionButton(),
         // note: Drawer 만들기
-        drawer: Drawer(
-            // child: ListTile(
-            //   leading: const Icon(Icons.change_history),
-            //   title: const Text('Change history'),
-            //   onTap: () {
-            //     // change app state...
-            //     Navigator.pop(context); // close the drawer
-            //   },
-            // ),
-            ),
-        // todo: 추후 appbar 분리하기
-        appBar: AppBar(
-          backgroundColor: Colors.cyan,
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
-          ],
-        ),
-
-        body: Column(
-          children: [
-            // todo: 추후 상단 body 분리하기
-            Container(
-              height: 80,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  children: [
-                    // note: 카테고리 list view 가로 설정
-                    Container(
-                      child: Expanded(
-                        child: ListView.separated(
-                          scrollDirection: Axis.horizontal,
-                          separatorBuilder: (context, index) {
-                            return SizedBox(width: 25);
-                          },
-                          itemCount: 5,
-                          itemBuilder: (context, index) {
-                            return Text('${categories[index]}');
-                          },
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 15),
-                    // note: 우측 끝: 목록 생성 버튼과 달력 버튼
-                    Container(
-                      child: Row(
-                        children: [
-                          // note: 목록 카테고리 생성 버튼
-                          IconButton(
-                              onPressed: () {},
-                              icon: Icon(Icons.list_alt_outlined)),
-                          // note: 달력 페이지로 이동하는 버튼
-                          IconButton(
-                              onPressed: () {},
-                              icon: Icon(Icons.calendar_month))
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            // todo: 하단 body 분리하기
-            Container(
-              color: Colors.green,
-              height: 600,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: GridView.count(
-                  crossAxisSpacing: 5.0,
-                  mainAxisSpacing: 5.0,
-                  crossAxisCount: 3,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(8),
-                      color: Colors.teal[100],
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child:
-                                const Text("표제 및 세부 내용 작성")
-                          ),
-                          Text('${DateTime.now()}')
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(8),
-                      color: Colors.teal[200],
-                      child: Column(
-                        children: [
-                          Expanded(child: const Text('Heed not the rabble')),
-                          Text('${DateTime.now()}')
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(8),
-                      color: Colors.teal[300],
-                      child: Column(
-                        children: [
-                          Expanded(
-                              child: const Text('Sound of screams but the')),
-                          Text('${DateTime.now()}')
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(8),
-                      color: Colors.teal[400],
-                      child: Column(
-                        children: [
-                          Expanded(child: const Text('Who scream')),
-                          Text('${DateTime.now()}')
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
+        drawer: DrawerWidget(),
       ),
     );
   }
+
+  AppBar buildAppBar() {
+    return AppBar(
+        backgroundColor: Colors.cyan,
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+        ],
+      );
+  }
+
+  FloatingActionButton buildFloatingActionButton() {
+    return FloatingActionButton.extended(
+      onPressed: () {},
+      label: const Text('Add'),
+      icon: const Icon(Icons.add),
+    );
+  }
+
 }
+
