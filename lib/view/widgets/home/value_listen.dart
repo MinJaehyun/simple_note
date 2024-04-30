099bb767ffc3e4fcf3a0f4360e7073d296ed7561
MinJaehyun <krism01@naver.com>
2024-04-30 18:08:03 +0900

    홈화면에 수정과 삭제 기능 구현

--- lib/view/widgets/home/value_listen.dart
+++ lib/view/widgets/home/value_listen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/helper/hive_helper.dart';
 import 'package:simple_note/model/memo.dart';
+import 'package:simple_note/view/screens/memo/update_memo.dart';
+
+enum SampleItem { updateMemo, deleteMemo }
 
 class ValueListenWidget extends StatefulWidget {
   const ValueListenWidget({super.key});
@@ -11,6 +14,8 @@ class ValueListenWidget extends StatefulWidget {
 }
 
 class _ValueListenWidgetState extends State<ValueListenWidget> {
+  SampleItem? selectedItem;
+
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
@@ -22,40 +27,68 @@ class _ValueListenWidgetState extends State<ValueListenWidget> {
           shrinkWrap: true,
           itemCount: box.values.length,
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
+            crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
             childAspectRatio: 1 / 1, //item 의 가로 1, 세로 1 의 비율
             mainAxisSpacing: 10, //수평 Padding
             crossAxisSpacing: 10, //수직 Padding
           ),
           itemBuilder: (BuildContext context, int index) {
             MemoModel? currentContact = box.getAt(index);
+            // note: 홈페이지 - 카드 클릭하면 해당 메모page로 이동함
             return Card(
               clipBehavior: Clip.antiAlias,
               child: InkWell(
-                onLongPress: () {
-                  /* ... */
+                onTap: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return UpdateMemo(
+                        index: index, currentContact: currentContact!);
+                  }));
                 },
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Text(currentContact!.title,
-                                overflow: TextOverflow.ellipsis),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              HiveHelper().delete(index);
-                            },
-                            child: Icon(Icons.delete_outline, size: 15),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 5),
-                      Text(currentContact.time.toString()),
-                    ],
+                  child: ListTile(
+                    titleAlignment: ListTileTitleAlignment.top,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 35.0, horizontal: 16.0),
+                    title: Text(currentContact!.title,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 20)),
+                    subtitle: Text(currentContact.time.toString(),
+                        style: TextStyle(color: Colors.grey.withOpacity(0.9))),
+                    trailing: Column(
+                      children: [
+                        PopupMenuButton<SampleItem>(
+                          initialValue: selectedItem,
+                          onSelected: (SampleItem item) {
+                            setState(() {
+                              selectedItem = item;
+                            });
+                          },
+                          itemBuilder: (BuildContext context) =>
+                              <PopupMenuEntry<SampleItem>>[
+                            PopupMenuItem<SampleItem>(
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => UpdateMemo(
+                                        index: index,
+                                        currentContact: currentContact),
+                                  ),
+                                );
+                              },
+                              value: SampleItem.updateMemo,
+                              child: Text('수정'),
+                            ),
+                            PopupMenuItem<SampleItem>(
+                              onTap: () => HiveHelper().delete(index),
+                              value: SampleItem.deleteMemo,
+                              child: Text('삭제'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
