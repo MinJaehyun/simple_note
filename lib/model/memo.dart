571f8aeee4a00d984091d068e956be903eecc417
MinJaehyun <krism01@naver.com>
2024-04-30 14:31:08 +0900

    주석제거

--- lib/model/memo.dart
+++ lib/model/memo.dart
@@ -1,12 +1,7 @@
-// id는 직접 지정하지 않을 것이다..
-// time
-// title
-// mainText: 본문
-
 import 'package:hive/hive.dart';
 part 'memo.g.dart';
 
-@HiveType(typeId: 0)
+@HiveType(typeId: 1)
 class MemoModel {
   @HiveField(0)
   final DateTime time;
@@ -17,9 +12,8 @@ class MemoModel {
   @HiveField(2)
   final String mainText;
 
-  MemoModel({required this.title, required this.time, required this.mainText});
+  MemoModel({required this.time, required this.title, required this.mainText});
 
-  // json 만들기
   MemoModel.fromJson(Map<String, dynamic> json)
       : time = json['time'],
         title = json['title'],
