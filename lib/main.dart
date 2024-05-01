6689ad39d720b6ab310d18ddb7aa04fc117eea19
MinJaehyun <krism01@naver.com>
2024-05-01 22:24:01 +0900

    카테고리 아답타 초기화 설정 및 카테고리 박스 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/helper/hive_helper.dart';
+import 'package:simple_note/helper/hive_helper_category.dart';
+import 'package:simple_note/model/category.dart';
 import 'package:simple_note/model/memo.dart';
 import 'view/screens/home/my_page.dart';
 
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(MemoModelAdapter());
+  Hive.registerAdapter(CategoryModelAdapter());
   await HiveHelper().openBox();
+  await HiveHelperCategory().openBox();
   runApp(const MyApp());
 }
 
