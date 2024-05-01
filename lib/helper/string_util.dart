39f9ae752217d7ca1722f664bf8fb7e249dbe124
MinJaehyun <krism01@naver.com>
2024-05-01 13:58:09 +0900

    데이터 포멧 2개로 설정하여 홈화면에는 간단하게 출력하고, 업데이트 화면에는 자세하게 출력

--- lib/helper/string_util.dart
+++ lib/helper/string_util.dart
@@ -1,8 +1,13 @@
 import 'package:intl/intl.dart';
 
 class FormatDate {
-  String formatDate(DateTime datetime) {
+  String formatDateTime(DateTime datetime) {
     // ==> '2024년 05월 01일  11:30:25AM'
     return DateFormat('yyyy${'년'} MM${'월'} dd${'일'} ').add_jms().format(datetime);
+  }
+
+  String formatDate(DateTime datetime) {
+    // ==> '2024년 05월 01일
+    return DateFormat('yyyy${'년'} MM${'월'} dd${'일'}').format(datetime);
   }
 }
