c3669b39f46d180fae14e00ea8f8b051a9f32be2
MinJaehyun <krism01@naver.com>
2024-05-04 18:40:54 +0900

    홈페이지에서 범주 DB를 가져와서 UI에 나타내기 위한 설정

--- lib/view/widgets/home/home_page_body_frame.dart
+++ lib/view/widgets/home/home_page_body_frame.dart
@@ -1,72 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:simple_note/model/category.dart';
 import 'package:simple_note/view/screens/category/category.dart';
 import 'package:simple_note/view/widgets/home/value_listen.dart';
+import 'package:simple_note/controller/hive_helper_category.dart';
 
 class HomePageBodyFrame extends StatelessWidget {
   const HomePageBodyFrame({
     super.key,
-    required this.categories,
   });
-
-  final List<String> categories;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 80,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                Container(
-                  child: Expanded(
-                    child: ListView.separated(
-                      scrollDirection: Axis.horizontal,
-                      separatorBuilder: (context, index) {
-                        return SizedBox(width: 10);
-                      },
-                      itemCount: 5,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          clipBehavior: Clip.antiAlias,
-                          child: InkWell(
-                            splashColor: Colors.blue.withAlpha(30),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Text('${categories[index]}'),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-                // note: 우측 끝: 목록 생성 버튼과 달력 버튼
-                Container(
-                  child: Row(
-                    children: [
-                      IconButton(
-                          onPressed: () {
-                            // todo:
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                              return AddCategory();
-                            },),);
+    return ValueListenableBuilder(
+      valueListenable: Hive.box<CategoryModel>(CategoryBox).listenable(),
+      builder: (context, Box<CategoryModel> box, _) {
+        if (box.values.isEmpty) return Center(child: Text('범주를 생성해 주세요'));
+        return Column(
+          children: [
+            Container(
+              height: 80,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Container(
+                      child: Expanded(
+                        child: ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          separatorBuilder: (context, index) {
+                            return SizedBox(width: 10);
                           },
-                          icon: Icon(Icons.list_alt_outlined)),
-                      IconButton(
-                          onPressed: () {}, icon: Icon(Icons.calendar_month))
-                    ],
-                  ),
-                )
-              ],
+                          itemCount: box.values.length,
+                          itemBuilder: (context, index) {
+                            CategoryModel? currentContact = box.getAt(index);
+                            return Card(
+                              clipBehavior: Clip.antiAlias,
+                              child: InkWell(
+                                splashColor: Colors.blue.withAlpha(30),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Text('${currentContact?.categories}'),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    // note: 우측 끝: 목록 생성 버튼과 달력 버튼
+                    Container(
+                      child: Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .push(MaterialPageRoute(builder: (context) {
+                                  return AddCategory();
+                                }));
+                              },
+                              icon: Icon(Icons.list_alt_outlined)),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.calendar_month),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
-          ),
-        ),
-        // note: 하단 body
-        ValueListenWidget(),
-      ],
+            // note: 하단 body
+            ValueListenWidget(),
+          ],
+        );
+      },
     );
   }
 }
