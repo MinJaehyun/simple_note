ac3f0c76ca8d38f5b7579d184731ae9d5cfb9e21
MinJaehyun <krism01@naver.com>
2024-05-05 21:04:47 +0900

    업데이트 메모 동적 범주에 맞게 구현

--- lib/view/screens/memo/update_memo.dart
+++ lib/view/screens/memo/update_memo.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/controller/hive_helper.dart';
+import 'package:simple_note/controller/hive_helper_category.dart';
 import 'package:simple_note/controller/string_util.dart';
+import 'package:simple_note/model/category.dart';
 import 'package:simple_note/model/memo.dart';
 
 class UpdateMemo extends StatefulWidget {
@@ -30,111 +33,118 @@ class _UpdateMemoState extends State<UpdateMemo> {
       }
     }
 
-    DropdownButton<String> DropdownButtonWidget() {
+    DropdownButton<String> DropdownButtonWidget(Box<CategoryModel> box) {
       return DropdownButton(
+        hint: Text('${widget.currentContact.selectedCategory}'),
         value: _dropdownValue.isNotEmpty ? _dropdownValue : null,
         onChanged: dropdownCallback,
-        items: [
-          DropdownMenuItem(child: Text('직장'), value: 'job'),
-          DropdownMenuItem(child: Text('감사일기'), value: 'thanksDairy'),
-          DropdownMenuItem(child: Text('일상'), value: 'today')
-        ],
+        items: box.values.toList().map<DropdownMenuItem<String>>((value) {
+          return DropdownMenuItem<String>(
+            value: value.categories,
+            child: Text(value.categories ?? '분류되지 않음'),
+          );
+        }).toList(),
         iconSize: 35,
       );
     }
 
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            // note: 상단 컨테이너
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.green,
-                height: 80,
-                child: Row(
-                  children: [
-                    Expanded(child: Text('${FormatDate().formatDateTime(widget.currentContact.time)}')),
-                    TextButton(
-                      onPressed: () {},
-                      child: DropdownButtonWidget(),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            // note: 중단 컨테이너
-            Expanded(
-              child: Form(
-                key: widget.formKey,
-                child: Padding(
+        body: ValueListenableBuilder(
+          valueListenable: Hive.box<CategoryModel>(CategoryBox).listenable(),
+          builder: (context, Box<CategoryModel> box, _) {
+            return Column(
+              children: [
+                // note: 상단 컨테이너
+                Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
                     color: Colors.green,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    height: 80,
+                    child: Row(
                       children: [
-                        TextFormField(
-                          autofocus: true,
-                          initialValue: widget.currentContact.title,
-                          decoration: InputDecoration(
-                            hintText: '제목을 입력해 주세요',
-                          ),
-                          onChanged: (value) {
-                            setState(() {
-                              title = value;
-                            });
-                          },
-                        ),
-                        TextFormField(
-                          initialValue: widget.currentContact.mainText,
-                          decoration: InputDecoration(
-                            hintText: '내용을 입력해 주세요',
-                          ),
-                          keyboardType: TextInputType.multiline,
-                          maxLines: null,
-                          onChanged: (value) {
-                            setState(() {
-                              mainText = value;
-                            });
-                          },
+                        Expanded(child: Text('${FormatDate().formatDateTime(widget.currentContact.time)}')),
+                        TextButton(
+                          onPressed: () {},
+                          child: DropdownButtonWidget(box),
                         ),
                       ],
                     ),
                   ),
                 ),
-              ),
-            ),
-            // note: save and cancel
-            Container(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // note: 입력값 변하지 않으면 저장해도 취소하여서 이전 입력값 남기도록 한다
-                        if(title != '' || mainText != '') {
-                          HiveHelper().updateMemo(index: widget.index, title: title, time: time, mainText: mainText);
-                        }
-                        Navigator.of(context).pop();
-                      },
-                      child: Text('저장'),
+                // note: 중단 컨테이너
+                Expanded(
+                  child: Form(
+                    key: widget.formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        color: Colors.green,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextFormField(
+                              autofocus: true,
+                              initialValue: widget.currentContact.title,
+                              decoration: InputDecoration(
+                                hintText: '제목을 입력해 주세요',
+                              ),
+                              onChanged: (value) {
+                                setState(() {
+                                  title = value;
+                                });
+                              },
+                            ),
+                            TextFormField(
+                              initialValue: widget.currentContact.mainText,
+                              decoration: InputDecoration(
+                                hintText: '내용을 입력해 주세요',
+                              ),
+                              keyboardType: TextInputType.multiline,
+                              maxLines: null,
+                              onChanged: (value) {
+                                setState(() {
+                                  mainText = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                  SizedBox(width: 15),
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Text('취소'),
-                    ),
+                ),
+                // note: save and cancel
+                Container(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // note: 입력값 변하지 않으면 저장해도 취소하여서 이전 입력값 남기도록 한다
+                            if(title != '' || mainText != '') {
+                              HiveHelper().updateMemo(_dropdownValue, index: widget.index, title: title, time: time, mainText: mainText);
+                            }
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('저장'),
+                        ),
+                      ),
+                      SizedBox(width: 15),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('취소'),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-          ],
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
