1efc4ea3b0a56b1d23483b17f2a9b69c6e13e8d0
MinJaehyun <krism01@naver.com>
2024-05-08 01:37:47 +0900

    intl 초기화 설정

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,19 @@ import 'package:simple_note/controller/hive_helper_category.dart';
 import 'package:simple_note/model/category.dart';
 import 'package:simple_note/model/memo.dart';
 import 'view/screens/home/my_page.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
 
 void main() async {
+  // note: hive 설정
   await Hive.initFlutter();
   Hive.registerAdapter(MemoModelAdapter());
   Hive.registerAdapter(CategoryModelAdapter());
   await HiveHelper().openBox();
   await HiveHelperCategory().openBox();
+
+  // note: intl 초기화
+  await initializeDateFormatting();
   runApp(const MyApp());
 }
 
