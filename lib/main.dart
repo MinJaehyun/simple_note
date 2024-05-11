13593f98b8eca07809092c6ad6914a79850057e6
MinJaehyun <krism01@naver.com>
2024-05-11 16:26:56 +0900

    theme - colorScheme, textTheme 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/controller/hive_helper.dart';
 import 'package:simple_note/controller/hive_helper_category.dart';
@@ -6,7 +7,6 @@ import 'package:simple_note/model/category.dart';
 import 'package:simple_note/model/memo.dart';
 import 'view/screens/home/my_page.dart';
 import 'package:intl/date_symbol_data_local.dart';
-
 
 void main() async {
   // note: hive 설정
@@ -26,7 +26,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0xff00ffff),
+          brightness: Brightness.light,
+        ),
+        textTheme: GoogleFonts.notoSansNKoTextTheme(
+          Theme.of(context).textTheme,
+        )
+      ),
       home: MyPage(),
     );
   }
