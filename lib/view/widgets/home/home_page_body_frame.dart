aa2108bfdb3c9aebef585403584802e001b61126
MinJaehyun <krism01@naver.com>
2024-05-12 19:07:53 +0900

    소프트키보드 포커싱 처리 및 검색 기능 구현 관련 설정

--- lib/view/widgets/home/home_page_body_frame.dart
+++ lib/view/widgets/home/home_page_body_frame.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:simple_note/controller/hive_helper.dart';
 import 'package:simple_note/model/category.dart';
-import 'package:simple_note/view/screens/category/category.dart';
+import 'package:simple_note/model/memo.dart';
+import 'package:simple_note/view/widgets/home/HomeSearchWidget.dart';
 import 'package:simple_note/view/widgets/home/homeSelectedCategoryWidget.dart';
 import 'package:simple_note/view/widgets/home/value_listen.dart';
 import 'package:simple_note/controller/hive_helper_category.dart';
@@ -15,109 +17,164 @@ class HomePageBodyFrame extends StatefulWidget {
 
 class _HomePageBodyFrameState extends State<HomePageBodyFrame> {
   String? selectedCategory;
+  var searchedTitle;
+  var searchedMainText;
+  String? searchControllerText;
+  TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     // var textStyle = Theme.of(context).textTheme;
-    return ValueListenableBuilder(
-      valueListenable: Hive.box<CategoryModel>(CategoryBox).listenable(),
-      builder: (context, Box<CategoryModel> box, _) {
-        // if (box.values.isEmpty) return Center(child: Text('범주를 생성해 주세요'));
-        return SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 148,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      // note: 2라인 미분류, 모든 메모, 우측 끝: 목록 생성 버튼과 달력 버튼
-                      Container(
-                        child: Row(
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Column(
+        children: [
+          ValueListenableBuilder(
+            valueListenable: Hive.box<CategoryModel>(CategoryBox).listenable(),
+            builder: (context, Box<CategoryModel> box, _) {
+              // if (box.values.isEmpty) return Center(child: Text('범주를 생성해 주세요'));
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 148,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
                           children: [
-                            Expanded(
+                            // 상단: 모든, 미분류, 검색창
+                            Container(
                               child: Row(
                                 children: [
-                                  Card(
-                                    child: TextButton(
-                                      onPressed: () {
-                                        setState(() {
-                                          setState(() {
-                                            selectedCategory = null;
-                                          });
-                                          print(selectedCategory);
-                                        });
-                                      },
-                                      child: Text('모든', style: TextStyle(color: Theme.of(context).colorScheme.primary)),
+                                  // 상단 모든, 미분류 박스
+                                  Expanded(
+                                    child: Row(
+                                      children: [
+                                        Card(
+                                          child: TextButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                setState(() {
+                                                  selectedCategory = null;
+                                                  searchControllerText = null;
+                                                  searchController.clear();
+                                                });
+                                              });
+                                            },
+                                            child: Text('모든', style: TextStyle(color: Theme.of(context).colorScheme.primary)),
+                                          ),
+                                        ),
+                                        Card(
+                                          child: TextButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                selectedCategory = '';
+                                                searchControllerText = null;
+                                                searchController.clear();
+                                              });
+                                            },
+                                            child: Text('미분류', style: TextStyle(color: Theme.of(context).colorScheme.primary)),
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                  Card(
-                                    child: TextButton(
-                                      onPressed: () {
-                                        setState(() {
-                                          selectedCategory = '';
-                                        });
-                                      },
-                                      child: Text('미분류', style: TextStyle(color: Theme.of(context).colorScheme.primary)),
+                                  // 상단 검색창
+                                  Container(
+                                    width: 220,
+                                    child: Form(
+                                      child: TextFormField(
+                                        controller: searchController,
+                                        decoration: InputDecoration(
+                                          hintText: '내용을 입력해 주세요',
+                                          contentPadding: EdgeInsets.all(12),
+                                          suffixIcon: Icon(Icons.search),
+                                          border: OutlineInputBorder(
+                                            borderRadius: BorderRadius.all(Radius.circular(4)),
+                                          ),
+                                        ),
+                                        onChanged: (value) {
+                                          setState(() {
+                                            searchController.text = value;
+                                            searchControllerText = searchController.text;
+                                          });
+                                        },
+                                        onTap: () {},
+                                        // validator: , enabled: ,
+                                      ),
                                     ),
                                   ),
                                 ],
                               ),
                             ),
-                            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-                            // note: 전체 범주
-                            IconButton(
-                              onPressed: () {
-                                Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                                  // todo:
-                                  return AllCategory();
-                                }));
-                              },
-                              icon: Icon(Icons.list_alt_outlined),
+                            // 상단: 생성한 카테고리
+                            Container(
+                              child: Expanded(
+                                child: ListView.separated(
+                                  scrollDirection: Axis.horizontal,
+                                  separatorBuilder: (context, index) {
+                                    return SizedBox(width: 0);
+                                  },
+                                  itemCount: box.values.length,
+                                  itemBuilder: (context, index) {
+                                    CategoryModel? categoryContact = box.getAt(index);
+                                    return Card(
+                                      clipBehavior: Clip.antiAlias,
+                                      child: TextButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            searchControllerText = null;
+                                            selectedCategory = categoryContact!.categories;
+                                            searchController.clear();
+                                          });
+                                        },
+                                        // note: categoryContact?.categories 에만 모든 범주가 담겨있다
+                                        child: Text('${categoryContact?.categories}'),
+                                        // child: Text('${categoryContact?.categories}', style: textStyle.titleMedium),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
                             ),
                           ],
                         ),
                       ),
-                      Container(
-                        child: Expanded(
-                          child: ListView.separated(
-                            scrollDirection: Axis.horizontal,
-                            separatorBuilder: (context, index) {
-                              return SizedBox(width: 0);
-                            },
-                            itemCount: box.values.length,
-                            itemBuilder: (context, index) {
-                              CategoryModel? categoryContact = box.getAt(index);
-                              return Card(
-                                clipBehavior: Clip.antiAlias,
-                                child: TextButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      selectedCategory = categoryContact!.categories;
-                                    });
-                                  },
-                                  // note: categoryContact?.categories 에만 모든 범주가 담겨있다
-                                  child: Text('${categoryContact?.categories}'),
-                                  // child: Text('${categoryContact?.categories}', style: textStyle.titleMedium),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          // note: 하단 body
+          ValueListenableBuilder(
+            valueListenable: Hive.box<MemoModel>(MemoBox).listenable(),
+            builder: (context, Box<MemoModel> box, _) {
+              return Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Container(
+                  height: 500,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        // todo: 분기 처리하는 과정 좀 더 고민하기 (한줄 이라도 없으면 효율 증가, 조건 많은 순으로 정렬하여 처리 )
+                        // 모든 카테고리 누르고 입력 내용 없으면 모든 메모 나타내기
+                        if (selectedCategory == null && searchControllerText == null) HomeAllCategoryWidget(),
+                        // 범주 있으면서, 검색내용 있으면, 아래 검색한 내용 나타내기(HomeSearchWidget)
+                        if (selectedCategory != null && searchControllerText != null) HomeSearchWidget(searchControllerText!),
+                        if (selectedCategory != null) HomeSelectedCategoryWidget(selectedCategory),
+                        if (searchControllerText != null) HomeSearchWidget(searchControllerText!),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              // note: 하단 body - 전체 나타내기
-              // note: 선택한 범주가 없으면 아래 실행하고, 선택한 범주가 있으면 아래 실행한다.
-              if (selectedCategory == null) HomeAllCategoryWidget(),
-              if (selectedCategory != null) HomeSelectedCategoryWidget(selectedCategory)
-            ],
+              );
+            },
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 }
