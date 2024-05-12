cd947c5543dba9bfa9507298a652c5ab96283a89
MinJaehyun <krism01@naver.com>
2024-05-13 00:13:03 +0900

    공통으로 사용할 네비게이션 분리

--- lib/view/screens/home/my_page.dart
+++ lib/view/screens/home/my_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:simple_note/view/screens/calendar/calendar.dart';
-import 'package:simple_note/view/screens/category/category.dart';
 import 'package:simple_note/view/screens/memo/add_memo.dart';
-import 'package:simple_note/view/widgets/home/home_page_body_frame.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:simple_note/view/widgets/home/home_page.dart';
+import 'package:simple_note/view/widgets/public/navigation_bar.dart';
 
 class MyPage extends StatefulWidget {
   const MyPage({Key? key}) : super(key: key);
@@ -13,9 +11,6 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> {
-  int currentIndex = 2;
-  bool isTapped = false;
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -27,106 +22,13 @@ class _MyPageState extends State<MyPage> {
         // note: Appbar
         appBar: buildAppBar(),
         // note: home page body frame
-        body: HomePageBodyFrame(),
+        body: HomePage(),
+        // note: navigation bar
+        bottomNavigationBar: BuildCurvedNavigationBar(2),
         // note: 하단 add
         floatingActionButton: buildFloatingActionButton(context),
-        // note: navigation bar
-        bottomNavigationBar: buildCurvedNavigationBar(),
       ),
     );
-  }
-
-  // todo: 추후, 중복 개선하기
-  CurvedNavigationBar buildCurvedNavigationBar() {
-    return CurvedNavigationBar(
-        backgroundColor: Colors.transparent,
-        animationDuration: Duration(milliseconds: 300),
-        index: currentIndex,
-        color: Theme.of(context).colorScheme.onPrimary,
-        items: [
-          // note: 캘린더
-          IconButton(
-            icon: Icon(Icons.calendar_month),
-            onPressed: (){
-              // setState(() {
-              //   isTapped = true;
-              //   currentIndex = 0;
-              // });
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return CalendarPage();
-              },));
-            },
-            iconSize: 25,
-            color: isTapped && currentIndex == 0 ? Colors.cyan : Colors.grey,
-          ),
-          // note: 범주
-          IconButton(
-            icon: Icon(Icons.category),
-            onPressed: (){
-              // setState(() {
-              //   isTapped = true;
-              //   currentIndex = 1;
-              // });
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return AllCategory();
-              },));
-            },
-            iconSize: 25,
-            color: isTapped && currentIndex == 1 ? Colors.cyan : Colors.grey,
-          ),
-          // note: 모든 메모
-          IconButton(
-            icon: Icon(Icons.data_array_outlined),
-            onPressed: (){
-              // setState(() {
-              //   isTapped = true;
-              //   currentIndex = 2;
-              // });
-              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {
-                return MyPage();
-              },));
-            },
-            iconSize: 25,
-            color: isTapped && currentIndex == 2 ? Colors.cyan : Colors.grey,
-          ),
-          // note: 설정
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: (){
-              setState(() {
-                isTapped = true;
-                currentIndex = 3;
-              });
-              // Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-              //   return;
-              // },));
-            },
-            iconSize: 25,
-            color: isTapped && currentIndex == 3 ? Colors.cyan : Colors.grey,
-          ),
-          // note: 휴지통
-          IconButton(
-            icon: Icon(Icons.delete),
-            onPressed: (){
-              setState(() {
-                isTapped = true;
-                currentIndex = 4;
-              });
-              // Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-              //   return;
-              // },));
-            },
-            iconSize: 25,
-            color: isTapped && currentIndex == 4 ? Colors.cyan : Colors.grey,
-          ),
-        ],
-        onTap: (index) {
-          setState(() {
-            currentIndex = index;
-            isTapped = true;
-          });
-        },
-      );
   }
 
   AppBar buildAppBar() {
@@ -135,7 +37,6 @@ class _MyPageState extends State<MyPage> {
       centerTitle: true,
     );
   }
-
   FloatingActionButton buildFloatingActionButton(context) {
     return FloatingActionButton.extended(
       onPressed: () {
