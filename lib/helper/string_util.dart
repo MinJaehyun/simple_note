725642017ef3bd875b3dbd16fac27ab1f59607b9
MinJaehyun <krism01@naver.com>
2024-05-14 18:25:04 +0900

    날짜 한영 구분하여 정렬

--- lib/helper/string_util.dart
+++ lib/helper/string_util.dart
@@ -1,17 +1,22 @@
 import 'package:intl/intl.dart';
 
 class FormatDate {
-  String formatDay(DateTime datetime) {
+  String formatDayEng(DateTime datetime) {
     // ==> Mon, 5/6/2024
     return DateFormat.yMEd().format(datetime);
   }
 
-  String formatDate(DateTime datetime) {
+  String formatDefaultDateKor(DateTime datetime) {
+    // ==> '2024.05.01
+    return DateFormat('yyyy.MM.dd').format(datetime);
+  }
+
+  String formatDateKor(DateTime datetime) {
     // ==> '2024년 05월 01일
     return DateFormat('yyyy${'년'} MM${'월'} dd${'일'}').format(datetime);
   }
 
-  String formatDateTime(DateTime datetime) {
+  String formatDateTimeKor(DateTime datetime) {
     // ==> '2024년 05월 01일  11:30:25AM'
     return DateFormat('yyyy${'년'} MM${'월'} dd${'일'} ').add_jms().format(datetime);
   }
