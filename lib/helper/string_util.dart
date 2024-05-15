dde4514daf78f5d1a1e997cd0c6768d4dc7740a5
MinJaehyun <krism01@naver.com>
2024-05-15 23:29:45 +0900

    설정 추가 및 명칭 변경

--- lib/helper/string_util.dart
+++ lib/helper/string_util.dart
@@ -2,22 +2,27 @@ import 'package:intl/intl.dart';
 
 class FormatDate {
   String formatDayEng(DateTime datetime) {
-    // ==> Mon, 5/6/2024
+    // Mon, 5/6/2024
     return DateFormat.yMEd().format(datetime);
   }
 
   String formatDefaultDateKor(DateTime datetime) {
-    // ==> '2024.05.01
+    // '2024.05.01
     return DateFormat('yyyy.MM.dd').format(datetime);
   }
 
+  String formatDotDateTimeKor(DateTime datetime) {
+    // '2024.05.01.3:24:27PM
+    return DateFormat('yyyy.MM.dd.').add_jms().format(datetime);
+  }
+
   String formatDateKor(DateTime datetime) {
-    // ==> '2024년 05월 01일
+    // '2024년 05월 01일
     return DateFormat('yyyy${'년'} MM${'월'} dd${'일'}').format(datetime);
   }
 
   String formatDateTimeKor(DateTime datetime) {
-    // ==> '2024년 05월 01일  11:30:25AM'
-    return DateFormat('yyyy${'년'} MM${'월'} dd${'일'} ').add_jms().format(datetime);
+    // '2024년 05월 01일 11:30:25AM'
+    return DateFormat('yyyy${'년'} MM${'월'} dd${'일'}').add_jms().format(datetime);
   }
 }
