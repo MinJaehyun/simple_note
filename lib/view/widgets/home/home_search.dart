fcff6371806ec7f2392abdd88cf0260e14e231a3
MinJaehyun <krism01@naver.com>
2024-05-16 11:18:00 +0900

    정렬 구현

--- lib/view/widgets/home/home_search.dart
+++ lib/view/widgets/home/home_search.dart
@@ -3,15 +3,17 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/controller/hive_helper_memo.dart';
 import 'package:simple_note/helper/string_util.dart';
 import 'package:simple_note/model/memo.dart';
+import 'package:simple_note/view/screens/home/my_page.dart';
 import 'package:simple_note/view/screens/memo/update_memo.dart';
 import 'package:substring_highlight/substring_highlight.dart';
 
 enum SampleItem { updateMemo, deleteMemo }
 
 class HomeSearchWidget extends StatefulWidget {
-  const HomeSearchWidget(this.searchControllerText, {super.key});
+  const HomeSearchWidget(this.searchControllerText, this.sortedTime, {super.key});
 
   final String searchControllerText;
+  final SortedTime? sortedTime;
 
   @override
   State<HomeSearchWidget> createState() => _HomeSearchWidgetState();
@@ -44,12 +46,15 @@ class _HomeSearchWidgetState extends State<HomeSearchWidget> {
             itemBuilder: (BuildContext context, int index) {
               // MemoModel? currentContact = box.getAt(index);
               MemoModel currentContact = boxSearchTitleAndMainText[index];
+              MemoModel? reversedCurrentContact = boxSearchTitleAndMainText[boxSearchTitleAndMainText.length -1 -index];
+              var sortedCard = widget.sortedTime == SortedTime.firstTime ? currentContact : reversedCurrentContact;
+
               return Card(
                 clipBehavior: Clip.antiAlias,
                 child: InkWell(
                   onTap: () {
                     Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                      return UpdateMemo(index: index, currentContact: currentContact);
+                      return UpdateMemo(index: index, currentContact: sortedCard);
                     }));
                   },
                   child: Padding(
@@ -58,7 +63,7 @@ class _HomeSearchWidgetState extends State<HomeSearchWidget> {
                       titleAlignment: ListTileTitleAlignment.top,
                       contentPadding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
                       title: SubstringHighlight(
-                        text: currentContact.title,
+                        text: sortedCard.title,
                         // 검색한 내용 가져오기
                         term: widget.searchControllerText,
                         // non-highlight style
@@ -71,7 +76,7 @@ class _HomeSearchWidgetState extends State<HomeSearchWidget> {
                           // decoration: TextDecoration.underline,
                         ),
                       ),
-                      subtitle: Text(FormatDate().formatDefaultDateKor(currentContact.createdAt), style: TextStyle(color: Colors.grey.withOpacity(0.9))),
+                      subtitle: Text(FormatDate().formatDefaultDateKor(sortedCard.createdAt), style: TextStyle(color: Colors.grey.withOpacity(0.9))),
                       // note: card() 내 수정, 삭제 버튼
                       trailing: Column(
                         children: [
@@ -87,7 +92,7 @@ class _HomeSearchWidgetState extends State<HomeSearchWidget> {
                                 onTap: () {
                                   Navigator.of(context).push(
                                     MaterialPageRoute(
-                                      builder: (context) => UpdateMemo(index: index, currentContact: currentContact),
+                                      builder: (context) => UpdateMemo(index: index, currentContact: sortedCard),
                                     ),
                                   );
                                 },
