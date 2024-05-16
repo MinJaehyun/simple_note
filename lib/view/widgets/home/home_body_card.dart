2f5680001178b802ad496b0920130e7d148b1ddb
MinJaehyun <krism01@naver.com>
2024-05-17 01:31:35 +0900

    PopupMenuButtonWidget 위젯으로 분리 및 업데이트 시 나타내는 화면 에러 수정

--- lib/view/widgets/home/home_body_card.dart
+++ lib/view/widgets/home/home_body_card.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/controller/hive_helper_memo.dart';
+import 'package:simple_note/helper/popup_menu_button_widget.dart';
 import 'package:simple_note/helper/string_util.dart';
 import 'package:simple_note/model/memo.dart';
 import 'package:simple_note/view/screens/home/my_page.dart';
 import 'package:simple_note/view/screens/memo/update_memo.dart';
-
-enum SampleItem { updateMemo, deleteMemo }
 
 class HomeBodyCardWidget extends StatefulWidget {
   const HomeBodyCardWidget(this.sortedTime, {super.key});
@@ -18,7 +17,7 @@ class HomeBodyCardWidget extends StatefulWidget {
 }
 
 class _HomeBodyCardWidgetState extends State<HomeBodyCardWidget> {
-  SampleItem? selectedItem;
+  var sortedCard;
 
   @override
   Widget build(BuildContext context) {
@@ -42,57 +41,30 @@ class _HomeBodyCardWidgetState extends State<HomeBodyCardWidget> {
             itemBuilder: (BuildContext context, int index) {
               // firstTime이면 오래된 순서로 정렬하고, lastTime이면 생성된 순서로 정렬한다.
               MemoModel? currentContact = box.getAt(index);
-              MemoModel? reversedCurrentContact = box.getAt(box.values.length -1 -index);
-              var sortedCard = widget.sortedTime == SortedTime.firstTime ? currentContact : reversedCurrentContact;
+              MemoModel? reversedCurrentContact = box.getAt(box.values.length - 1 - index);
+              sortedCard = widget.sortedTime == SortedTime.firstTime ? currentContact : reversedCurrentContact;
 
               return Card(
                 clipBehavior: Clip.antiAlias,
                 child: InkWell(
                   onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                      return UpdateMemo(index: index, currentContact: sortedCard);
-                    }));
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) {
+                        // fix: 업데이트 할 때에는 currentContact: sortedCard 넣으면 에러
+                        return UpdateMemo(index: index, currentContact: currentContact!);
+                      }),
+                    );
                   },
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: ListTile(
-                      titleAlignment: ListTileTitleAlignment.top,
-                      contentPadding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
-                      title: Text(sortedCard!.title, overflow: TextOverflow.ellipsis, style: style),
-                      subtitle:
-                          Text(FormatDate().formatDefaultDateKor(sortedCard.createdAt), style: TextStyle(color: Colors.grey.withOpacity(0.9))),
-                      // note: card() 내 수정, 삭제 버튼
-                      trailing: Column(
-                        children: [
-                          PopupMenuButton<SampleItem>(
-                            initialValue: selectedItem,
-                            onSelected: (SampleItem item) {
-                              setState(() {
-                                selectedItem = item;
-                              });
-                            },
-                            itemBuilder: (BuildContext context) => <PopupMenuEntry<SampleItem>>[
-                              PopupMenuItem<SampleItem>(
-                                onTap: () {
-                                  Navigator.of(context).push(
-                                    MaterialPageRoute(
-                                      builder: (context) => UpdateMemo(index: index, currentContact: sortedCard),
-                                    ),
-                                  );
-                                },
-                                value: SampleItem.updateMemo,
-                                child: Text('수정'),
-                              ),
-                              PopupMenuItem<SampleItem>(
-                                onTap: () => HiveHelperMemo().delete(index),
-                                value: SampleItem.deleteMemo,
-                                child: Text('삭제'),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+                        titleAlignment: ListTileTitleAlignment.top,
+                        contentPadding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
+                        title: Text(sortedCard!.title, overflow: TextOverflow.ellipsis, style: style),
+                        subtitle:
+                            Text(FormatDate().formatDefaultDateKor(sortedCard.createdAt), style: TextStyle(color: Colors.grey.withOpacity(0.9))),
+                        // note: card() 내 수정, 삭제 버튼
+                        trailing: PopupMenuButtonWidget(index, sortedCard)),
                   ),
                 ),
               );
