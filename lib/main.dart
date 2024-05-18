d08cf73ada0bcb8ec3a726a7732d1baf794ceae4
MinJaehyun <krism01@naver.com>
2024-05-18 16:41:38 +0900

    dark mode: 기본 main 설정

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,23 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/controller/hive_helper_memo.dart';
 import 'package:simple_note/controller/hive_helper_category.dart';
-// import 'package:simple_note/controller/hive_helper_sort.dart';
 import 'package:simple_note/model/category.dart';
 import 'package:simple_note/model/memo.dart';
-// import 'package:simple_note/model/sort.dart';
 import 'view/screens/home/my_page.dart';
 import 'package:intl/date_symbol_data_local.dart';
+
+// todo: 추후 hive_helper_dark_mode 파일로 분리하기
+const darkModeBox = 'darkModel';
 
 void main() async {
   // note: hive 설정
   await Hive.initFlutter();
   Hive.registerAdapter(MemoModelAdapter());
   Hive.registerAdapter(CategoryModelAdapter());
-  // Hive.registerAdapter(SortModelAdapter());
   await HiveHelperMemo().openBox();
   await HiveHelperCategory().openBox();
-  // await HiveHelperSort().openBox();
+  await Hive.openBox(darkModeBox);
+
   // note: intl 초기화
   await initializeDateFormatting();
   runApp(const MyApp());
@@ -29,18 +30,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Color(0xff00ffff),
-          brightness: Brightness.light,
-        ),
-        textTheme: GoogleFonts.notoSansNKoTextTheme(
-          Theme.of(context).textTheme,
-        )
-      ),
-      home: MyPage(),
+    // 다크모드 설정을 위한 ValueListenableBuilder 설정
+    return ValueListenableBuilder(
+      valueListenable: Hive.box(darkModeBox).listenable(),
+      builder: (context, box, widget) {
+        var darkMode = box.get('darkMode', defaultValue: false);
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          // 다크모드 설정
+          themeMode: darkMode ? ThemeMode.dark : ThemeMode.light,
+          darkTheme: ThemeData.dark(),
+          // theme 내에 2가지 설정 (colorScheme, textTheme)
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Color(0xff00ffff),
+              brightness: Brightness.light,
+            ),
+            textTheme: GoogleFonts.notoSansNKoTextTheme(
+              ThemeData.light().textTheme,
+            ),
+          ),
+          home: MyPage(),
+        );
+      },
     );
   }
 }
