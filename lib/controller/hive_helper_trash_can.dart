7ee90b17180015f2bbbbeb793e3616c07f9eec39
MinJaehyun <krism01@naver.com>
2024-05-20 20:05:01 +0900

    불필요한 기능 삭제

--- lib/controller/hive_helper_trash_can.dart
+++ lib/controller/hive_helper_trash_can.dart
@@ -19,7 +19,7 @@ class HiveHelperTrashCan {
 
   // note: CRUD
   Future read() async {
-    return trashCanBox!.values.toList();
+    return trashCanBox!.values.toList();가
   }
 
   // todo: 추후: 시작과 끝나는 시간 속성 넣기 DateTime startTime, DateTime endTime :
@@ -31,26 +31,8 @@ class HiveHelperTrashCan {
     trashCanBox!.putAt(index, TrashCanModel(createdAt: createdAt, title: title, mainText: mainText, selectedCategory: selectedCategory));
   }
 
-  // Future addMemo({required String title, required DateTime time, required String mainText, required String category}) async {
-  //   return trashCanBox!.add(TrashCanModel(time: time, title: title, mainText: mainText, category: category));
-  // }
-  //
-  // Future updateMemo({required index, required String title, required DateTime time, required String mainText, required String category}) async {
-  //   trashCanBox!.putAt(index, TrashCanModel(time: time, title: title, mainText: mainText, category: category));
-  // }
-
   Future delete(int index) async {
     trashCanBox!.deleteAt(index);
   }
-
-// note: 미사용
-// Future create(TrashCanModel memo) async {
-//   return trashCanBox!.add(memo);
-// }
-
-// note: 미사용
-// Future update(int index, TrashCanModel updateMomo) async {
-//   trashCanBox!.putAt(index, updateMomo);
-// }
 
 }
