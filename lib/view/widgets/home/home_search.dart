0b796b91241e6b891f4825d6962a4ad4824a709a
MinJaehyun <krism01@naver.com>
2024-05-21 19:58:17 +0900

    중복 코드 리펙토링

--- lib/view/widgets/home/home_search.dart
+++ lib/view/widgets/home/home_search.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/controller/hive_helper_memo.dart';
+import 'package:simple_note/view/widgets/home/popup_menu_button_widget.dart';
 import 'package:simple_note/helper/string_util.dart';
 import 'package:simple_note/model/memo.dart';
 import 'package:simple_note/view/screens/home/my_page.dart';
-import 'package:simple_note/view/screens/memo/update_memo.dart';
+import 'package:simple_note/view/screens/crud_memo/update_memo.dart';
 import 'package:substring_highlight/substring_highlight.dart';
 
 enum SampleItem { updateMemo, deleteMemo }
@@ -12,7 +13,7 @@ enum SampleItem { updateMemo, deleteMemo }
 class HomeSearchWidget extends StatefulWidget {
   const HomeSearchWidget(this.searchControllerText, this.sortedTime, {super.key});
 
-  final String searchControllerText;
+  final String? searchControllerText;
   final SortedTime? sortedTime;
 
   @override
@@ -22,6 +23,13 @@ class HomeSearchWidget extends StatefulWidget {
 class _HomeSearchWidgetState extends State<HomeSearchWidget> {
   SampleItem? selectedItem;
   late List<MemoModel> boxSearchTitleAndMainText;
+  // late String _dropdownValue;
+  //
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _dropdownValue = '미분류';
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -30,84 +38,65 @@ class _HomeSearchWidgetState extends State<HomeSearchWidget> {
       builder: (context, Box<MemoModel> box, _) {
         if (box.values.isEmpty) return Center(child: Text('우측 하단 버튼을 클릭하여 메모를 생성해 주세요'));
         boxSearchTitleAndMainText = box.values.where((item) {
-          return item.title.contains(widget.searchControllerText) || item.mainText!.contains(widget.searchControllerText);
+          return item.title.contains(widget.searchControllerText!) || item.mainText!.contains(widget.searchControllerText!);
         }).toList();
+
         return Container(
           height: MediaQuery.of(context).size.height - 200,
           child: GridView.builder(
             shrinkWrap: true,
             itemCount: boxSearchTitleAndMainText.length,
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
-              childAspectRatio: 1 / 1, //item 의 가로 1, 세로 1 의 비율
-              mainAxisSpacing: 10, //수평 Padding
-              crossAxisSpacing: 10, //수직 Padding
+              crossAxisCount: 2,
+              childAspectRatio: 1/1,
+              mainAxisSpacing: 0,
+              crossAxisSpacing: 0,
             ),
             itemBuilder: (BuildContext context, int index) {
-              // MemoModel? currentContact = box.getAt(index);
+              // note: MemoModel? currentContact = box.getAt(index);
               MemoModel currentContact = boxSearchTitleAndMainText[index];
-              MemoModel? reversedCurrentContact = boxSearchTitleAndMainText[boxSearchTitleAndMainText.length -1 -index];
+              MemoModel? reversedCurrentContact = boxSearchTitleAndMainText[boxSearchTitleAndMainText.length - 1 - index];
               var sortedCard = widget.sortedTime == SortedTime.firstTime ? currentContact : reversedCurrentContact;
 
               return Card(
                 clipBehavior: Clip.antiAlias,
                 child: InkWell(
                   onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                      return UpdateMemo(index: index, currentContact: sortedCard);
-                    }));
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) {
+                        return UpdateMemo(index: index, currentContact: sortedCard);
+                      }),
+                    );
                   },
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: ListTile(
                       titleAlignment: ListTileTitleAlignment.top,
                       contentPadding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
-                      title: SubstringHighlight(
-                        text: sortedCard.title,
-                        // 검색한 내용 가져오기
-                        term: widget.searchControllerText,
-                        // non-highlight style
-                        textStyle: TextStyle(color: Colors.grey),
-                        // highlight style
-                        textStyleHighlight: TextStyle(
-                          fontSize: 24.0,
-                          color: Colors.black,
-                          backgroundColor: Colors.yellow,
-                          // decoration: TextDecoration.underline,
-                        ),
-                      ),
-                      subtitle: Text(FormatDate().formatDefaultDateKor(sortedCard.createdAt), style: TextStyle(color: Colors.grey.withOpacity(0.9))),
-                      // note: card() 내 수정, 삭제 버튼
-                      trailing: Column(
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          PopupMenuButton<SampleItem>(
-                            initialValue: selectedItem,
-                            onSelected: (SampleItem item) {
-                              setState(() {
-                                selectedItem = item;
-                              });
-                            },
-                            itemBuilder: (BuildContext context) => <PopupMenuEntry<SampleItem>>[
-                              PopupMenuItem<SampleItem>(
-                                onTap: () {
-                                  Navigator.of(context).push(
-                                    MaterialPageRoute(
-                                      builder: (context) => UpdateMemo(index: index, currentContact: sortedCard),
-                                    ),
-                                  );
-                                },
-                                value: SampleItem.updateMemo,
-                                child: Text('수정'),
-                              ),
-                              PopupMenuItem<SampleItem>(
-                                onTap: () => HiveHelperMemo().delete(index),
-                                value: SampleItem.deleteMemo,
-                                child: Text('삭제'),
-                              ),
-                            ],
+                          SizedBox(height: 10.0),
+                          SubstringHighlight(
+                            text: sortedCard.title,
+                            // 검색한 내용 가져오기
+                            term: widget.searchControllerText,
+                            // non-highlight style
+                            textStyle: TextStyle(color: Colors.grey),
+                            // highlight style
+                            textStyleHighlight: TextStyle(
+                              fontSize: 24.0,
+                              color: Colors.black,
+                              backgroundColor: Colors.yellow,
+                              // decoration: TextDecoration.underline,
+                            ),
                           ),
+                          SizedBox(height: 100),
+                          Text(FormatDate().formatDefaultDateKor(sortedCard.createdAt), style: TextStyle(color: Colors.grey.withOpacity(0.9))),
                         ],
                       ),
+                      // note: card() 내 수정, 삭제 버튼
+                      trailing: PopupMenuButtonWidget(index, sortedCard),
                     ),
                   ),
                 ),
