04a1cd1ae5ef48faee760a7ce30b8fe220e264dd
MinJaehyun <krism01@naver.com>
2024-05-25 04:15:24 +0900

    격자 구현

---
+++ lib/helper/grid_painter.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class GridPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.black12
+      ..strokeWidth = 1.0;
+
+    const double gridSize = 20.0;
+
+    // 가로 선 그리기
+    for (double i = 0; i < size.height; i += gridSize) {
+      canvas.drawLine(Offset(0, i), Offset(size.width, i), paint);
+    }
+
+    // 세로 선 그리기
+    for (double i = 0; i < size.width; i += gridSize) {
+      canvas.drawLine(Offset(i, 0), Offset(i, size.height), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
