296644e3787a043aa94f9b18b280a1dc727abbfe
MinJaehyun <krism01@naver.com>
2024-05-25 00:04:46 +0900

    공통: 네비게이션바 휴지통과 셋업창 위치 변경 및 GetX 설정 및 snackbar 적용

--- lib/view/widgets/public/footer_navigation_bar_widget.dart
+++ lib/view/widgets/public/footer_navigation_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:get/route_manager.dart';
 import 'package:simple_note/view/screens/calendar/calendar_page.dart';
 import 'package:simple_note/view/screens/category/category_page.dart';
 import 'package:simple_note/view/screens/memo/memo_page.dart';
@@ -41,13 +42,15 @@ class _FooterNavigationBarWidgetState extends State<FooterNavigationBarWidget> {
         IconButton(
           tooltip: '달력',
           icon: const Icon(Icons.calendar_month),
-          onPressed: (){
+          onPressed: () {
             setState(() {
               setTappedAndIndex(0);
             });
-            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {
-              return const CalendarPage();
-            },));
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) {
+                return const CalendarPage();
+              },
+            ));
           },
           iconSize: 25,
           color: currentIndex == 0 ? Colors.cyan : Colors.grey,
@@ -56,13 +59,15 @@ class _FooterNavigationBarWidgetState extends State<FooterNavigationBarWidget> {
         IconButton(
           tooltip: '범주',
           icon: const Icon(Icons.category),
-          onPressed: (){
+          onPressed: () {
             setState(() {
               setTappedAndIndex(1);
             });
-            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {
-              return const CategoryPage();
-            },));
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) {
+                return const CategoryPage();
+              },
+            ));
           },
           iconSize: 25,
           color: currentIndex == 1 ? Colors.cyan : Colors.grey,
@@ -71,45 +76,70 @@ class _FooterNavigationBarWidgetState extends State<FooterNavigationBarWidget> {
         IconButton(
           tooltip: '모든 메모',
           icon: const Icon(Icons.data_array_outlined),
-          onPressed: (){
+          onPressed: () {
             setState(() {
               setTappedAndIndex(2);
             });
-            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {
-              return const MemoPage();
-            },));
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) {
+                return const MemoPage();
+              },
+            ));
           },
           iconSize: 25,
           color: currentIndex == 2 ? Colors.cyan : Colors.grey,
-        ),
-        // note: 설정
-        IconButton(
-          tooltip: '설정',
-          icon: const Icon(Icons.settings),
-          onPressed: (){
-            setState(() {
-              isTapped = true;
-              currentIndex = 3;
-            });
-            // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {
-            //   return;
-            // },));
-          },
-          iconSize: 25,
-          color: currentIndex == 3 ? Colors.cyan : Colors.grey,
         ),
         // note: 휴지통
         IconButton(
           tooltip: '휴지통',
           icon: const Icon(Icons.delete),
-          onPressed: (){
+          onPressed: () {
+            setState(() {
+              isTapped = true;
+              currentIndex = 3;
+            });
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) {
+                return const TrashCanPage();
+              },
+            ));
+          },
+          iconSize: 25,
+          color: currentIndex == 3 ? Colors.cyan : Colors.grey,
+        ),
+        // note: 설정
+        IconButton(
+          tooltip: '설정',
+          icon: const Icon(Icons.settings),
+          onPressed: () {
             setState(() {
               isTapped = true;
               currentIndex = 4;
             });
-            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {
-              return const TrashCanPage();
-            },));
+            // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {
+            //   return;
+            // },));
+            Get.snackbar(
+              '기능을 준비 중 입니다',
+              '업데이트 준비 중: \n'
+                  '1. 구글 로그인 및 구글 동기화, \n'
+                  '2. 메모장 글꼴, 스킨 선택 기능 \n'
+                  '3. 즐겨 찾기 \n'
+                  '4. 태그 \n'
+                  '5. 달력 내 알림 및 타임라인 \n \n'
+                  '등등 많은 기능을 다듬고 있습니다. 좀 더 기다려 주세요 :) \n',
+              colorText: Colors.orange,
+              snackPosition: SnackPosition.BOTTOM,
+              forwardAnimationCurve: Curves.elasticInOut,
+              reverseAnimationCurve: Curves.easeOut,
+              duration: const Duration(seconds: 6),
+              mainButton: TextButton(
+                onPressed: () {
+                  Get.back(); // Closes the snackbar
+                },
+                child: const Text('Close', style: TextStyle(color: Colors.red)),
+              ),
+            );
           },
           iconSize: 25,
           color: currentIndex == 4 ? Colors.cyan : Colors.grey,
@@ -124,5 +154,3 @@ class _FooterNavigationBarWidgetState extends State<FooterNavigationBarWidget> {
     );
   }
 }
-
-
