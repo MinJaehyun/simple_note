fa4ef013df52d7d60bb526381dfc7b663d22a98c
MinJaehyun <krism01@naver.com>
2024-05-25 00:06:10 +0900

    GetMaterialApp 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/controller/hive_helper_memo.dart';
@@ -27,7 +28,7 @@ void main() async {
 
   // note: intl 초기화
   await initializeDateFormatting();
-  runApp(const MyApp());
+  runApp(const GetMaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
