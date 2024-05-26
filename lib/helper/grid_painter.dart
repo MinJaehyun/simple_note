2f53fad7ca922eeff34794a9a15ccf815e5c0109
MinJaehyun <krism01@naver.com>
2024-05-27 00:34:49 +0900

    grid painter 삼항 연산자로 격자 유무 설정

--- lib/helper/grid_painter.dart
+++ lib/helper/grid_painter.dart
@@ -5,24 +5,27 @@ import 'package:simple_note/main.dart';
 class GridPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    // note: print(Hive.box(themeModeBox).values);
+    // print(Hive.box(themeModeBox).values);
     // note: (true, false) 이 생성한 메모 개수 만큼 6개 출력된다.. ListView.builder 에서 itemCount 개수만큼 사용하므로 6개 출력한다
     // note: 즉, themeModeBox 박스에는 (true, false) 이 담기게 된다.
+    // 리스트의 첫 번째 요소는 darkmode, 두 번째 요소는 gridmode 가 담겨있다.
 
     final paint = Paint()
-    // note: 다크모드면 연한 흰색 나타내기
-      ..color = Hive.box(themeModeBox).values.last == false ? Colors.black12 : Colors.white12
+      // note: 다크모드면 연한 흰색 나타내기
+      ..color = Hive.box(themeModeBox).values.first == false ? Colors.black12 : Colors.white12
       ..strokeWidth = 1.0;
 
     const double gridSize = 20.0;
+    final bool isGridVisible = Hive.box(themeModeBox).values.last;
 
     // 가로 선 그리기
     for (double i = 0; i < size.height; i += gridSize) {
-      canvas.drawLine(Offset(0, i), Offset(size.width, i), paint);
+      // canvas.drawLine(Offset(0, i), Offset(size.width, i), paint); // 변경 전
+      isGridVisible ? canvas.drawLine(Offset(0, i), Offset(size.width, i), paint) : null;
     }
     // 세로 선 그리기
     for (double i = 0; i < size.width; i += gridSize) {
-      canvas.drawLine(Offset(i, 0), Offset(i, size.height), paint);
+      isGridVisible ? canvas.drawLine(Offset(i, 0), Offset(i, size.height), paint) : null;
     }
   }
 
