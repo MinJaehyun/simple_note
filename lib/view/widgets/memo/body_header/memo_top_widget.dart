df00e8e3c67352d16ba17cfd53e2a68804ac889a
MinJaehyun <krism01@naver.com>
2024-05-28 01:34:15 +0900

    메모 디자인 변경

--- lib/view/widgets/memo/body_header/memo_top_widget.dart
+++ lib/view/widgets/memo/body_header/memo_top_widget.dart
@@ -29,7 +29,7 @@ class _MemoTopWidgetState extends State<MemoTopWidget> {
 
   @override
   Widget build(BuildContext context) {
-    TextStyle style = TextStyle(color: Theme.of(context).colorScheme.primary);
+    // TextStyle style = TextStyle(color: Theme.of(context).colorScheme.primary);
 
     return GestureDetector(
       onTap: () {
@@ -37,142 +37,158 @@ class _MemoTopWidgetState extends State<MemoTopWidget> {
       },
       child: Column(
         children: [
-          // body_top: 범주 및 검색창
+          // body_top: 검색창 및 범주
           ValueListenableBuilder(
             valueListenable: Hive.box<CategoryModel>(CategoryBox).listenable(),
             builder: (context, Box<CategoryModel> box, _) {
               return SingleChildScrollView(
                 child: Container(
-                  height: 148,
+                  height: 155,
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: Column(
                       children: [
-                        // 상단: 모든, 미분류, 검색창
-                        Row(
-                          children: [
-                            // 상단1~2: 모든, 미분류 박스
-                            Expanded(
-                              child: Row(
-                                children: [
-                                  Card(
-                                    child: TextButton(
-                                      onPressed: () {
-                                        setState(() {
-                                          selectedCategory = null;
-                                          searchControllerText = null;
-                                          searchController.clear();
-                                        });
-                                      },
-                                      child: Text('모든', style: style),
-                                    ),
+                        // 상단1: 검색창
+                        SizedBox(
+                          width: double.infinity,
+                          child: Form(
+                            child: TextFormField(
+                              autofocus: false,
+                              controller: searchController,
+                              decoration: InputDecoration(
+                                focusedBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.orange,
+                                    width: 1,
                                   ),
-                                  Card(
-                                    child: TextButton(
-                                      onPressed: () {
-                                        setState(() {
-                                          selectedCategory = '미분류';
-                                          searchControllerText = null;
-                                          searchController.clear();
-                                        });
-                                      },
-                                      child: Text('미분류', style: style),
-                                    ),
+                                ),
+                                prefixIcon: GestureDetector(
+                                  onTap: () {},
+                                  child: const Icon(
+                                    Icons.search,
+                                    size: 24,
                                   ),
-                                ],
-                              ),
-                            ),
-                            // 상단3: 검색창
-                            SizedBox(
-                              width: 220,
-                              child: Form(
-                                child: TextFormField(
-                                  autofocus: false,
-                                  controller: searchController,
-                                  decoration: InputDecoration(
-                                    border: const OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(Radius.circular(4)),
-                                    ),
-                                    enabledBorder: const OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Colors.grey,
-                                        width: 1,
-                                      ),
-                                    ),
-                                    focusedBorder: const OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Colors.orange,
-                                        width: 1,
-                                      ),
-                                    ),
-                                    prefixIcon: GestureDetector(
-                                      onTap: () {},
-                                      child: const Icon(
-                                        Icons.search,
-                                        size: 24,
-                                      ),
-                                    ),
-                                    prefixIconColor: Colors.grey,
-                                    suffixIcon: GestureDetector(
-                                      onTap: () {
-                                        setState(() {
-                                          searchController.clear();
-                                          searchControllerText = null;
-                                        });
-                                      },
-                                      child: const Icon(
-                                        Icons.close,
-                                        size: 24,
-                                      ),
-                                    ),
-                                    suffixIconColor: Colors.grey,
-                                    hintText: '검색',
-                                    contentPadding: const EdgeInsets.all(12),
-                                    hintStyle: TextStyle(
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                  cursorColor: Colors.grey,
-                                  onChanged: (value) {
+                                ),
+                                prefixIconColor: Colors.grey,
+                                suffixIcon: GestureDetector(
+                                  onTap: () {
                                     setState(() {
-                                      searchController.text = value;
-                                      searchControllerText = searchController.text;
+                                      searchController.clear();
+                                      searchControllerText = null;
                                     });
                                   },
-                                  onTap: () {},
+                                  child: searchControllerText != null ? Icon(Icons.close, size: 24) : Icon(Icons.transit_enterexit),
+                                ),
+                                suffixIconColor: Colors.grey,
+                                hintText: '검색',
+                                contentPadding: const EdgeInsets.all(12),
+                                hintStyle: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                              cursorColor: Colors.grey,
+                              onChanged: (value) {
+                                setState(() {
+                                  searchController.text = value;
+                                  searchControllerText = searchController.text;
+                                });
+                              },
+                              onTap: () {},
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        // 상단2: 모든, 미분류, 생성한 범주
+                        Row(
+                          children: [
+                            Card(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  color: Colors.grey,
+                                  width: 1.0,
+                                ),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              // note: 선택한 범주가 '모든(null)' 이면 빨강 박스로
+                              color: selectedCategory == null ? Colors.red : null,
+                              child: TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    selectedCategory = null;
+                                    searchControllerText = null;
+                                    searchController.clear();
+                                  });
+                                },
+                                child: Text(
+                                  '모든',
+                                  // note: 선택한 범주가 '모든(null)' 이면 ...
+                                  style: TextStyle(color: selectedCategory == null ? Colors.white : null),
                                 ),
                               ),
                             ),
-                          ],
-                        ),
-
-                        // 상단: 생성한 카테고리
-                        Container(
-                          child: Expanded(
-                            child: ListView.separated(
-                              scrollDirection: Axis.horizontal,
-                              separatorBuilder: (context, index) {
-                                return const SizedBox(width: 0);
-                              },
-                              itemCount: box.values.length,
-                              itemBuilder: (context, index) {
-                                CategoryModel? categoryContact = box.getAt(index);
-                                return Card(
-                                  clipBehavior: Clip.antiAlias,
-                                  child: TextButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        searchControllerText = null;
-                                        searchController.clear();
-                                        selectedCategory = categoryContact!.categories;
-                                      });
-                                    },
-                                    child: Text('${categoryContact?.categories}'),
-                                  ),
-                                );
-                              },
+                            Card(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  color: Colors.grey,
+                                  width: 1.0,
+                                ),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              color: selectedCategory == '미분류' ? Colors.red : null,
+                              child: TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    selectedCategory = '미분류';
+                                    searchControllerText = null;
+                                    searchController.clear();
+                                  });
+                                },
+                                child: Text(
+                                  '미분류',
+                                  style: TextStyle(color: selectedCategory == '미분류' ? Colors.white : null),
+                                ),
+                              ),
                             ),
-                          ),
+                            // 상단: 생성한 카테고리
+                            SizedBox(
+                              width: 230,
+                              height: 60,
+                              child: ListView.separated(
+                                scrollDirection: Axis.horizontal,
+                                separatorBuilder: (context, index) {
+                                  return const SizedBox();
+                                },
+                                itemCount: box.values.length,
+                                itemBuilder: (context, index) {
+                                  CategoryModel? categoryContact = box.getAt(index);
+                                  return Card(
+                                    shape: RoundedRectangleBorder(
+                                      side: BorderSide(
+                                        color: Colors.grey,
+                                        width: 1.0,
+                                      ),
+                                      borderRadius: BorderRadius.circular(8.0),
+                                    ),
+                                    color: selectedCategory == categoryContact!.categories ? Colors.red : null,
+                                    clipBehavior: Clip.antiAlias,
+                                    child: TextButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          searchControllerText = null;
+                                          searchController.clear();
+                                          selectedCategory = categoryContact.categories;
+                                        });
+                                      },
+                                      child: Text(
+                                        '${categoryContact.categories}',
+                                        style: TextStyle(color: selectedCategory == categoryContact.categories ? Colors.white : null),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
