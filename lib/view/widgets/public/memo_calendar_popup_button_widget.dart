d92dbb760d3312d779c1dab43b075786ae738901
MinJaehyun <krism01@naver.com>
2024-06-07 01:01:49 +0900

    메모: 역정렬 후, 삭제 시 잘못된 index를 삭제하고 있다 (반대로 삭제하면 된다. (box.length - 1 - index))

--- lib/view/widgets/public/memo_calendar_popup_button_widget.dart
+++ lib/view/widgets/public/memo_calendar_popup_button_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:simple_note/controller/memo_controller.dart';
+import 'package:simple_note/controller/settings_controller.dart';
 import 'package:simple_note/controller/trash_can_memo_controller.dart';
 import 'package:simple_note/model/memo.dart';
 import 'package:simple_note/view/screens/public_crud_memo_calendar/update_memo_page.dart';
@@ -8,10 +9,10 @@ import 'package:simple_note/view/screens/public_crud_memo_calendar/update_memo_p
 enum SampleItem { updateMemo, deleteMemo }
 
 class MemoCalendarPopupButtonWidget extends StatefulWidget {
-  const MemoCalendarPopupButtonWidget(this.index, this.currentContact, {super.key});
+  const MemoCalendarPopupButtonWidget(this.index, this.sortedCard, {super.key});
 
   final int index;
-  final MemoModel currentContact;
+  final MemoModel sortedCard;
 
   @override
   State<MemoCalendarPopupButtonWidget> createState() => _MemoCalendarPopupButtonWidgetState();
@@ -41,7 +42,7 @@ class _MemoCalendarPopupButtonWidgetState extends State<MemoCalendarPopupButtonW
           onTap: () {
             Navigator.of(context).push(
               MaterialPageRoute(
-                builder: (context) => UpdateMemoPage(index: widget.index, currentContact: widget.currentContact),
+                builder: (context) => UpdateMemoPage(index: widget.index, currentContact: widget.sortedCard),
               ),
             );
           },
@@ -60,21 +61,21 @@ class _MemoCalendarPopupButtonWidgetState extends State<MemoCalendarPopupButtonW
                         onPressed: () {
                           // 휴지통에 담기
                           trashCanMemoController.addCtr(
-                            createdAt: widget.currentContact.createdAt,
-                            title: widget.currentContact.title,
-                            mainText: widget.currentContact.mainText,
+                            createdAt: widget.sortedCard.createdAt,
+                            title: widget.sortedCard.title,
+                            mainText: widget.sortedCard.mainText,
                             // note: 휴지통에 저장되면서 범주는 '미분류'로 지정되야 한다.
                             selectedCategory: _dropdownValue,
                             isFavoriteMemo: false,
                           );
                           // 일반 메모장에서 삭제하기
-                          memoController.deleteCtr(index: widget.index);
+                          // todo: 리펙토링하기: 바로 Hive 접근 대신, CTR 접근해서 가져오기
+                          memoController.deleteCtr(
+                              index: widget.sortedCard == SortedTime.firstTime ? widget.index : memoController.memoList.length - widget.index - 1);
                           Navigator.pop(context);
                         },
                         child: const Text('삭제')),
-                    TextButton(
-                        onPressed: () => Navigator.pop(context),
-                        child: const Text('취소')),
+                    TextButton(onPressed: () => Navigator.pop(context), child: const Text('취소')),
                   ],
                   elevation: 24.0,
                 );
