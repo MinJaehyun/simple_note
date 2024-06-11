86688198fc102636e9feb399cb879f001babb3ff
MinJaehyun <krism01@naver.com>
2024-06-11 18:33:28 +0900

    메모: 즉시 반영되지 않는 에러 발생 (add, update, read는 되는데 delete가 즉시 반영되지 않고 있다. delete는 popupButton으로 구성되어 있다 )

--- lib/view/widgets/memo/body_footer/memo_card_widget.dart
+++ lib/view/widgets/memo/body_footer/memo_card_widget.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/controller/memo_controller.dart';
-import 'package:simple_note/repository/local_data_source/memo_repository.dart';
 import 'package:simple_note/controller/settings_controller.dart';
 import 'package:simple_note/helper/grid_painter.dart';
 import 'package:simple_note/helper/string_util.dart';
@@ -21,25 +19,39 @@ class _MemoCardWidgetState extends State<MemoCardWidget> {
   final settingsController = Get.find<SettingsController>();
   final memoController = Get.find<MemoController>();
 
+  // note: 아래 2개의 bool 타입은 사용중인 요소이다
+  late bool _isCheckedTodo;
+  late bool _isFavoriteMemo;
+
   @override
   Widget build(BuildContext context) {
+    // fixme: 정렬은 아래처럼 하는게 아니다.. 시간 순으로 정렬하는 것이다.. 대규모 수정 예상..
+    List<MemoModel> sortedMemoList = List.from(memoController.memoList)..sort((a, b) => a.createdAt.compareTo(b.createdAt));
+    List<MemoModel> reverseSortedMemoList = List.from(memoController.memoList)..sort((a, b) => b.createdAt.compareTo(a.createdAt));
     TextStyle style = TextStyle(fontSize: 20, color: Theme.of(context).colorScheme.primary);
-    return ValueListenableBuilder(
-      valueListenable: Hive.box<MemoModel>(MemoBox).listenable(),
-      builder: (context, Box<MemoModel> box, _) {
-        if (box.values.isEmpty)
+
+    // return ValueListenableBuilder(
+    //   valueListenable: Hive.box<MemoModel>(MemoBox).listenable(),
+    //   builder: (context, Box<MemoModel> box, _) {
+    return Obx(() {
+        if (memoController.memoList.isEmpty) {
           return Column(
             children: [
               SizedBox(height: 200),
               Text('메모를 생성해 주세요'),
             ],
           );
+        }
+
+        // note: 선택된 정렬에 따라 올바른 리스트를 선택합니다.
+        List<MemoModel> selectedMemoList = settingsController.sortedTime == SortedTime.firstTime ? sortedMemoList : reverseSortedMemoList;
+
 
         return SizedBox(
           height: MediaQuery.of(context).size.height - 200,
           child: GridView.builder(
             shrinkWrap: true,
-            itemCount: box.values.length,
+            itemCount: selectedMemoList.length,
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
               childAspectRatio: 1 / 1, //item 의 가로 1, 세로 1 의 비율
@@ -47,11 +59,8 @@ class _MemoCardWidgetState extends State<MemoCardWidget> {
               crossAxisSpacing: 0, //수직 Padding
             ),
             itemBuilder: (BuildContext context, int index) {
-              // 정렬 설정: firstTime는 오래된 순서로 정렬, lastTime는 생성된 순서로 정렬
-              MemoModel? currentContact = box.getAt(index);
-              MemoModel? reversedCurrentContact = box.getAt(box.values.length - 1 - index);
-              MemoModel? sortedCard = settingsController.sortedTime == SortedTime.firstTime ? currentContact : reversedCurrentContact;
-              int sortedIndex = settingsController.sortedTime == SortedTime.firstTime ? index : box.values.length - 1 - index;
+              MemoModel? currentContact = selectedMemoList[index];
+              var sortedIndex = memoController.memoList.indexOf(currentContact);
 
               return Card(
                 shape: RoundedRectangleBorder(
@@ -68,10 +77,7 @@ class _MemoCardWidgetState extends State<MemoCardWidget> {
                     onTap: () {
                       Navigator.of(context).push(
                         MaterialPageRoute(builder: (context) {
-                          // 업데이트 시, 선택한 정렬에 따른 sortedCard 넣어서 출력
-                          return UpdateMemoPage(
-                              index: sortedIndex,
-                              sortedCard: sortedCard);
+                          return UpdateMemoPage(index: sortedIndex, sortedCard: currentContact);
                         }),
                       );
                     },
@@ -85,7 +91,7 @@ class _MemoCardWidgetState extends State<MemoCardWidget> {
                           children: [
                             Row(
                               children: [
-                                // todo: 추후, 구글 로그인 이미지 넣기
+                                // todo: 추후, 구글 로그인 이미지 넣기: memoController.memoList[index]
                                 IconButton(
                                   onPressed: () {},
                                   icon: const Icon(Icons.account_box),
@@ -98,13 +104,15 @@ class _MemoCardWidgetState extends State<MemoCardWidget> {
                                 const SizedBox(width: 10.0),
                                 Expanded(
                                   child: Text(
-                                    sortedCard!.title,
+                                    // memoController.memoList[index].title,
+                                    currentContact.title,
                                     overflow: TextOverflow.ellipsis,
                                     style: style,
                                   ),
                                 ),
                                 // card() 내 수정, 삭제 버튼
-                                MemoCalendarPopupButtonWidget(index, sortedCard),
+                                MemoCalendarPopupButtonWidget(sortedIndex, currentContact),
+
                               ],
                             ),
                             // todo: 아래 ?내용 넣기
@@ -113,7 +121,8 @@ class _MemoCardWidgetState extends State<MemoCardWidget> {
                               children: [
                                 Expanded(
                                   child: Text(
-                                    FormatDate().formatSimpleTimeKor(sortedCard.createdAt),
+                                    // FormatDate().formatSimpleTimeKor(memoController.memoList[index].createdAt),
+                                    FormatDate().formatSimpleTimeKor(currentContact.createdAt),
                                     style: TextStyle(color: Colors.grey.withOpacity(0.9)),
                                   ),
                                 ),
@@ -124,18 +133,24 @@ class _MemoCardWidgetState extends State<MemoCardWidget> {
                                   constraints: BoxConstraints(),
                                   // 기본 제약조건 제거
                                   visualDensity: VisualDensity(horizontal: -4.0),
-                                  icon: sortedCard.isCheckedTodo == false
+
+                                  // icon: memoController.memoList[index].isCheckedTodo == false
+                                  icon: currentContact.isCheckedTodo == false
                                       ? const Icon(Icons.check_box_outline_blank)
                                       : const Icon(Icons.check_box, color: Colors.red),
                                   onPressed: () {
+                                    setState(() {
+                                      _isCheckedTodo = !memoController.memoList[index].isCheckedTodo!;
+                                    });
                                     memoController.updateCtr(
-                                      index: settingsController.sortedTime == SortedTime.firstTime ? index : box.values.length - index - 1,
-                                      createdAt: sortedCard.createdAt,
-                                      title: sortedCard.title,
-                                      mainText: sortedCard.mainText,
-                                      selectedCategory: sortedCard.selectedCategory,
-                                      isFavoriteMemo: sortedCard.isFavoriteMemo!,
-                                      isCheckedTodo: !sortedCard.isCheckedTodo!,
+                                      // index: settingsController.sortedTime == SortedTime.firstTime ? index : box.values.length - index - 1,
+                                      index: memoController.memoList.indexOf(currentContact),
+                                      createdAt: currentContact.createdAt,
+                                      title: currentContact.title,
+                                      mainText: currentContact.mainText,
+                                      selectedCategory: currentContact.selectedCategory,
+                                      isFavoriteMemo: currentContact.isFavoriteMemo!,
+                                      isCheckedTodo: !currentContact.isCheckedTodo!,
                                     );
                                   },
                                 ),
@@ -145,18 +160,22 @@ class _MemoCardWidgetState extends State<MemoCardWidget> {
                                   constraints: BoxConstraints(), // 기본 제약조건 제거
                                   // visualDensity: VisualDensity(horizontal: -4.0),
                                   // 동적 처리
-                                  icon: sortedCard.isFavoriteMemo == false
+                                  icon: currentContact.isFavoriteMemo == false
                                       ? const Icon(Icons.star_border_sharp, color: null)
                                       : const Icon(Icons.star, color: Colors.red),
                                   onPressed: () {
+                                    setState(() {
+                                      _isFavoriteMemo = !memoController.memoList[index].isFavoriteMemo!;
+                                    });
                                     memoController.updateCtr(
-                                      index: settingsController.sortedTime == SortedTime.firstTime ? index : box.values.length - index - 1,
-                                      createdAt: sortedCard.createdAt,
-                                      title: sortedCard.title,
-                                      mainText: sortedCard.mainText,
-                                      selectedCategory: sortedCard.selectedCategory,
-                                      isFavoriteMemo: !sortedCard.isFavoriteMemo!,
-                                      isCheckedTodo: sortedCard.isCheckedTodo!,
+                                      // index: index,
+                                      index: memoController.memoList.indexOf(currentContact),
+                                      createdAt: currentContact.createdAt,
+                                      title: currentContact.title,
+                                      mainText: currentContact.mainText,
+                                      selectedCategory: currentContact.selectedCategory,
+                                      isFavoriteMemo: !currentContact.isFavoriteMemo!,
+                                      isCheckedTodo: currentContact.isCheckedTodo!,
                                     );
                                   },
                                 ),
--- lib/view/widgets/public/memo_calendar_popup_button_widget.dart
+++ lib/view/widgets/public/memo_calendar_popup_button_widget.dart
@@ -35,61 +35,130 @@ class _MemoCalendarPopupButtonWidgetState extends State<MemoCalendarPopupButtonW
   Widget build(BuildContext context) {
     return PopupMenuButton<SampleItem>(
       initialValue: selectedItem,
-      onSelected: (SampleItem item) {
+      onSelected: (SampleItem item) async {
         setState(() => selectedItem = item);
-      },
-      itemBuilder: (BuildContext context) => <PopupMenuEntry<SampleItem>>[
-        PopupMenuItem<SampleItem>(
-          child: const Text('수정'),
-          value: SampleItem.updateMemo,
-          onTap: () {
+
+        switch(item) {
+          case SampleItem.updateMemo:
             Navigator.of(context).push(
               MaterialPageRoute(builder: (context) {
                 return UpdateMemoPage(index: widget.index, sortedCard: widget.sortedCard);
               }),
             );
-          },
-        ),
-        PopupMenuItem<SampleItem>(
-          child: const Text('삭제'),
-          value: SampleItem.deleteMemo,
-          onTap: () {
-            showDialog(
+            break;
+
+          case SampleItem.deleteMemo:
+            bool? result = await showDialog(
               context: context,
               builder: (context) {
-                return AlertDialog(
-                  title: const Text("메모를 삭제 하시겠습니까?"),
-                  elevation: 24.0,
-                  actions: [
-                    TextButton(
-                      child: const Text('삭제'),
-                      onPressed: () {
-                        // 휴지통에 담기
-                        trashCanMemoController.addCtr(
-                          createdAt: widget.sortedCard.createdAt,
-                          title: widget.sortedCard.title,
-                          mainText: widget.sortedCard.mainText,
-                          // note: 휴지통에 저장되면서 범주는 '미분류'로 지정되야 한다.
-                          selectedCategory: _dropdownValue,
-                          isFavoriteMemo: false,
-                        );
-                        // 일반 메모장에서 삭제하기
-                        memoController.deleteCtr(
-                          index: settingsController.sortedTime == SortedTime.firstTime
-                              ? widget.index
-                              : memoController.memoList.length - widget.index - 1,
-                        );
-                        Navigator.pop(context);
-                      },
-                    ),
-                    TextButton(onPressed: () => Navigator.pop(context), child: const Text('취소')),
-                  ],
+                return StatefulBuilder(
+                  builder: (context, setState) {
+                    return AlertDialog(
+                      title: const Text("메모를 삭제 하시겠습니까?"),
+                      elevation: 24.0,
+                      actions: [
+                        TextButton(
+                          child: const Text('삭제'),
+                          onPressed: () {
+                            // 일반 메모장에서 삭제하기
+                            memoController.deleteCtr(index: widget.index);
+
+                            // 휴지통에 담기
+                            trashCanMemoController.addCtr(
+                              createdAt: widget.sortedCard.createdAt,
+                              title: widget.sortedCard.title,
+                              mainText: widget.sortedCard.mainText,
+                              selectedCategory: _dropdownValue,
+                              isFavoriteMemo: false,
+                            );
+
+                            Navigator.pop(context, true); // 삭제 확인
+                          },
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, false), // 삭제 취소
+                          child: const Text('취소'),
+                        ),
+                      ],
+                    );
+                  },
                 );
               },
             );
-          },
+
+            if (result == true) {
+              // UI 업데이트 로직을 추가할 수 있습니다.
+              setState(() {});
+            }
+            break;
+        }
+      },
+
+      itemBuilder: (BuildContext context) => <PopupMenuEntry<SampleItem>>[
+        PopupMenuItem<SampleItem>(
+          value: SampleItem.updateMemo,
+          child: const Text('수정'),
+        ),
+        PopupMenuItem<SampleItem>(
+          value: SampleItem.deleteMemo,
+          child: const Text('삭제'),
         ),
       ],
+
+      // 변경 전:
+      // itemBuilder: (BuildContext context) => <PopupMenuEntry<SampleItem>>[
+      //   PopupMenuItem<SampleItem>(
+      //     child: const Text('수정'),
+      //     value: SampleItem.updateMemo,
+      //     onTap: () {
+      //       Navigator.of(context).push(
+      //         MaterialPageRoute(builder: (context) {
+      //           return UpdateMemoPage(index: widget.index, sortedCard: widget.sortedCard);
+      //         }),
+      //       );
+      //     },
+      //   ),
+      //   PopupMenuItem<SampleItem>(
+      //     child: StatefulBuilder(
+      //       builder: (context, setState) {
+      //         return const Text('삭제');
+      //       },
+      //     ),
+      //     value: SampleItem.deleteMemo,
+      //     onTap: () {
+      //       showDialog(
+      //         context: context,
+      //         builder: (context) {
+      //           return AlertDialog(
+      //             title: const Text("메모를 삭제 하시겠습니까?"),
+      //             elevation: 24.0,
+      //             actions: [
+      //               TextButton(
+      //                 child: const Text('삭제'),
+      //                 onPressed: () {
+      //                   // 휴지통에 담기
+      //                   trashCanMemoController.addCtr(
+      //                     createdAt: widget.sortedCard.createdAt,
+      //                     title: widget.sortedCard.title,
+      //                     mainText: widget.sortedCard.mainText,
+      //                     // note: 휴지통에 저장되면서 범주는 '미분류'로 지정되야 한다.
+      //                     selectedCategory: _dropdownValue,
+      //                     isFavoriteMemo: false,
+      //                   );
+      //                   // 일반 메모장에서 삭제하기
+      //                   memoController.deleteCtr(index: widget.index);
+      //                   setState(() {});
+      //                   Navigator.pop(context);
+      //                 },
+      //               ),
+      //               TextButton(onPressed: () => Navigator.pop(context), child: const Text('취소')),
+      //             ],
+      //           );
+      //         },
+      //       );
+      //     },
+      //   ),
+      // ],
     );
   }
 }
