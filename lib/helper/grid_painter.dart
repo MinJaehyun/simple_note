4e170611c872a570fa27b452d0d3911032a9780c
MinJaehyun <krism01@naver.com>
2024-06-16 02:53:06 +0900

    error: 테마 모드 변경 시, 즉시 UI에 반영하지 않고 있다

--- lib/helper/grid_painter.dart
+++ lib/helper/grid_painter.dart
@@ -1,49 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:simple_note/main.dart';
+import 'package:get/get.dart';
+import 'package:simple_note/controller/settings_controller.dart';
 
 // todo: 코드 개선하기 (isGridVisible는 격자 설정 변수로써, controll => repository 사용하여 리펙토링하기)
 class GridPainter extends CustomPainter {
-  // final settingsCtr = Get.find<SettingsController>();
-  // themeModeBox는 여러 종류이므로 dynamic 설정함
-  Box<dynamic> box = Hive.box(themeModeBox);
-  bool isGridVisible = true;
+  final settingsController = Get.find<SettingsController>();
 
   @override
   void paint(Canvas canvas, Size size) {
-    // print(Hive.box(themeModeBox).values);
     // note: (true, false) 이 생성한 메모 개수 만큼 6개 출력된다.. ListView.builder 에서 itemCount 개수만큼 사용하므로 6개 출력한다
-    // note: 즉, themeModeBox 박스에는 (true, false) 이 담기게 된다.
-    // 리스트의 첫 번째 요소는 darkmode, 두 번째 요소는 gridmode 가 담겨있다.
 
-    final paint = Paint()
+    final paint = Paint();
       // note: 다크모드면 연한 흰색 나타내기
-      ..color = box.values.first == false ? Colors.black12 : Colors.white12
-      ..strokeWidth = 1.0;
+      //  box.values.first == false
+    // fixme:
+    paint.color = settingsController.isGridMode == false ? Colors.black12 : Colors.white12;
+    // paint.color = settingsController.isGridMode == true ? Colors.black12 : Colors.white12;
+    paint.strokeWidth = 1.0;
 
     const double gridSize = 20.0;
 
     // note: 인덱스로 접근하면 themeMode가 추가될 수록 바뀌므로, 키 값으로 지정 해야한다.
     // note: 아래 [2]는 gridMode 키의 값을 가져오면 된다(gridMode: true)
-    // 변경 전: isGridVisible = Hive.box(themeModeBox).values.toList()[2];
-    // 변경 후:
-    for (var key in box.keys) {
-      var value = box.get(key);
-      // print('$key: $value');
-      if (key == 'gridMode') {
-        isGridVisible = value;
-      }
-    }
+    // 변경 전: 삭제함.. 240616
+    // for (var key in box.keys) {
+    //   var value = box.get(key);
+    //   // print('$key: $value');
+    //   if (key == 'gridMode') {
+    //     // settingsController.isGridMode = value;
+    //     settingsController.updateAppbarFavoriteMemo();
+    //   }
+    // }
 
     // 가로 선 그리기
     for (double i = 0; i < size.height; i += gridSize) {
       // canvas.drawLine(Offset(0, i), Offset(size.width, i), paint); // 변경 전
-      isGridVisible ? canvas.drawLine(Offset(0, i), Offset(size.width, i), paint) : null;
+      settingsController.isGridMode == true ? canvas.drawLine(Offset(0, i), Offset(size.width, i), paint) : null;
     }
     // 세로 선 그리기
     for (double i = 0; i < size.width; i += gridSize) {
       // canvas.drawLine(Offset(i, 0), Offset(i, size.height), paint);
-      isGridVisible ? canvas.drawLine(Offset(i, 0), Offset(i, size.height), paint) : null;
+      settingsController.isGridMode == true ? canvas.drawLine(Offset(i, 0), Offset(i, size.height), paint) : null;
     }
   }
 
