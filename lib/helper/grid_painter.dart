3a6c94ec347ecfc6c0a54a9d097080e1d38d4cc4
MinJaehyun <krism01@naver.com>
2024-06-16 19:10:34 +0900

    obx 적용 및 grid 오류 해결

--- lib/helper/grid_painter.dart
+++ lib/helper/grid_painter.dart
@@ -1,46 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/controller/settings_controller.dart';
+import 'package:simple_note/main.dart';
 
-// todo: 코드 개선하기 (isGridVisible는 격자 설정 변수로써, controll => repository 사용하여 리펙토링하기)
 class GridPainter extends CustomPainter {
-  final settingsController = Get.find<SettingsController>();
+  final settingsCtr = Get.find<SettingsController>();
+  // themeModeBox는 여러 종류이므로 dynamic 설정함
+  Box<dynamic> box = Hive.box(themeModeBox);
+  bool isGridVisible = true;
 
   @override
   void paint(Canvas canvas, Size size) {
-    // note: (true, false) 이 생성한 메모 개수 만큼 6개 출력된다.. ListView.builder 에서 itemCount 개수만큼 사용하므로 6개 출력한다
-
-    final paint = Paint();
-      // note: 다크모드면 연한 흰색 나타내기
-      //  box.values.first == false
-    // fixme:
-    paint.color = settingsController.isGridMode == false ? Colors.black12 : Colors.white12;
-    // paint.color = settingsController.isGridMode == true ? Colors.black12 : Colors.white12;
-    paint.strokeWidth = 1.0;
+    final paint = Paint()
+    // note: 다크모드면 연한 흰색 나타내기(isGridMode 아니다!)
+      ..color = settingsCtr.isThemeMode == true ? Colors.black12 : Colors.white12
+      ..strokeWidth = 1.0;
 
     const double gridSize = 20.0;
 
     // note: 인덱스로 접근하면 themeMode가 추가될 수록 바뀌므로, 키 값으로 지정 해야한다.
     // note: 아래 [2]는 gridMode 키의 값을 가져오면 된다(gridMode: true)
-    // 변경 전: 삭제함.. 240616
-    // for (var key in box.keys) {
-    //   var value = box.get(key);
-    //   // print('$key: $value');
-    //   if (key == 'gridMode') {
-    //     // settingsController.isGridMode = value;
-    //     settingsController.updateAppbarFavoriteMemo();
-    //   }
-    // }
+    // 변경 전: isGridVisible = Hive.box(themeModeBox).values.toList()[2];
+    // 변경 후:
+    for (var key in box.keys) {
+      var value = box.get(key);
+      // print('$key: $value');
+      if (key == 'isGridMode') {
+        isGridVisible = value;
+      }
+    }
 
     // 가로 선 그리기
     for (double i = 0; i < size.height; i += gridSize) {
       // canvas.drawLine(Offset(0, i), Offset(size.width, i), paint); // 변경 전
-      settingsController.isGridMode == true ? canvas.drawLine(Offset(0, i), Offset(size.width, i), paint) : null;
+      isGridVisible ? canvas.drawLine(Offset(0, i), Offset(size.width, i), paint) : null;
     }
     // 세로 선 그리기
     for (double i = 0; i < size.width; i += gridSize) {
       // canvas.drawLine(Offset(i, 0), Offset(i, size.height), paint);
-      settingsController.isGridMode == true ? canvas.drawLine(Offset(i, 0), Offset(i, size.height), paint) : null;
+      isGridVisible ? canvas.drawLine(Offset(i, 0), Offset(i, size.height), paint) : null;
     }
   }
 
--- lib/view/widgets/memo/body_footer/memo_card_widget.dart
+++ lib/view/widgets/memo/body_footer/memo_card_widget.dart
@@ -90,105 +90,107 @@ class _MemoCardWidgetState extends State<MemoCardWidget> {
                     borderRadius: BorderRadius.circular(4.0),
                   ),
                   clipBehavior: Clip.antiAlias,
-                  child: CustomPaint(
-                    painter: GridPainter(),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) {
-                            return UpdateMemoPage(index: sortedIndex, sortedCard: currentContact);
-                          }),
-                        );
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: ListTile(
-                          titleAlignment: ListTileTitleAlignment.titleHeight,
-                          contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
-                          title: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  // todo: 추후, 구글 로그인 이미지 넣기: memoController.memoList[index]
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(Icons.account_box),
-                                    padding: EdgeInsets.zero,
-                                    // 패딩 설정
-                                    constraints: const BoxConstraints(),
-                                    iconSize: 50,
-                                    color: Colors.grey,
-                                  ),
-                                  const SizedBox(width: 10.0),
-                                  Expanded(
-                                    child: Text(
-                                      currentContact.title,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: style,
+                  child: Obx(() =>
+                    CustomPaint(
+                      painter: GridPainter(),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) {
+                              return UpdateMemoPage(index: sortedIndex, sortedCard: currentContact);
+                            }),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: ListTile(
+                            titleAlignment: ListTileTitleAlignment.titleHeight,
+                            contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    // todo: 추후, 구글 로그인 이미지 넣기: memoController.memoList[index]
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.account_box),
+                                      padding: EdgeInsets.zero,
+                                      // 패딩 설정
+                                      constraints: const BoxConstraints(),
+                                      iconSize: 50,
+                                      color: Colors.grey,
                                     ),
-                                  ),
-                                  // card() 내 수정, 삭제 버튼
-                                  MemoCalendarPopupButtonWidget(sortedIndex, currentContact),
-                                ],
-                              ),
-                              // todo: 아래 ?내용 넣기
-                              const SizedBox(height: 90.0),
-                              Row(
-                                children: [
-                                  Expanded(
-                                    child: Text(
-                                      FormatDate().formatSimpleTimeKor(currentContact.createdAt),
-                                      style: TextStyle(color: Colors.grey.withOpacity(0.9)),
+                                    const SizedBox(width: 10.0),
+                                    Expanded(
+                                      child: Text(
+                                        currentContact.title,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: style,
+                                      ),
                                     ),
-                                  ),
-                                  // 체크 버튼
-                                  IconButton(
-                                    padding: EdgeInsets.zero,
-                                    // 아이콘 버튼 내부의 패딩 제거
-                                    constraints: BoxConstraints(),
-                                    // 기본 제약조건 제거
-                                    visualDensity: VisualDensity(horizontal: -4.0),
-                                    icon: currentContact.isCheckedTodo == false
-                                        ? const Icon(Icons.check_box_outline_blank)
-                                        : const Icon(Icons.check_box, color: Colors.red),
-                                    onPressed: () {
-                                      memoController.updateCtr(
-                                        index: memoController.memoList.indexOf(currentContact),
-                                        createdAt: currentContact.createdAt,
-                                        title: currentContact.title,
-                                        mainText: currentContact.mainText,
-                                        selectedCategory: currentContact.selectedCategory,
-                                        isFavoriteMemo: currentContact.isFavoriteMemo!,
-                                        isCheckedTodo: !currentContact.isCheckedTodo!,
-                                      );
-                                    },
-                                  ),
-                                  // 즐겨 찾기
-                                  IconButton(
-                                    padding: EdgeInsets.zero,
-                                    constraints: BoxConstraints(),
-                                    icon: currentContact.isFavoriteMemo == false
-                                        ? const Icon(Icons.star_border_sharp, color: null)
-                                        : const Icon(Icons.star, color: Colors.red),
-                                    onPressed: () {
-                                      setState(() {
-                                        _isFavoriteMemo = !memoController.memoList[index].isFavoriteMemo!;
-                                      });
-                                      memoController.updateCtr(
-                                        index: memoController.memoList.indexOf(currentContact),
-                                        createdAt: currentContact.createdAt,
-                                        title: currentContact.title,
-                                        mainText: currentContact.mainText,
-                                        selectedCategory: currentContact.selectedCategory,
-                                        isFavoriteMemo: !currentContact.isFavoriteMemo!,
-                                        isCheckedTodo: currentContact.isCheckedTodo!,
-                                      );
-                                    },
-                                  ),
-                                ],
-                              ),
-                            ],
+                                    // card() 내 수정, 삭제 버튼
+                                    MemoCalendarPopupButtonWidget(sortedIndex, currentContact),
+                                  ],
+                                ),
+                                // todo: 아래 ?내용 넣기
+                                const SizedBox(height: 90.0),
+                                Row(
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        FormatDate().formatSimpleTimeKor(currentContact.createdAt),
+                                        style: TextStyle(color: Colors.grey.withOpacity(0.9)),
+                                      ),
+                                    ),
+                                    // 체크 버튼
+                                    IconButton(
+                                      padding: EdgeInsets.zero,
+                                      // 아이콘 버튼 내부의 패딩 제거
+                                      constraints: BoxConstraints(),
+                                      // 기본 제약조건 제거
+                                      visualDensity: VisualDensity(horizontal: -4.0),
+                                      icon: currentContact.isCheckedTodo == false
+                                          ? const Icon(Icons.check_box_outline_blank)
+                                          : const Icon(Icons.check_box, color: Colors.red),
+                                      onPressed: () {
+                                        memoController.updateCtr(
+                                          index: memoController.memoList.indexOf(currentContact),
+                                          createdAt: currentContact.createdAt,
+                                          title: currentContact.title,
+                                          mainText: currentContact.mainText,
+                                          selectedCategory: currentContact.selectedCategory,
+                                          isFavoriteMemo: currentContact.isFavoriteMemo!,
+                                          isCheckedTodo: !currentContact.isCheckedTodo!,
+                                        );
+                                      },
+                                    ),
+                                    // 즐겨 찾기
+                                    IconButton(
+                                      padding: EdgeInsets.zero,
+                                      constraints: BoxConstraints(),
+                                      icon: currentContact.isFavoriteMemo == false
+                                          ? const Icon(Icons.star_border_sharp, color: null)
+                                          : const Icon(Icons.star, color: Colors.red),
+                                      onPressed: () {
+                                        setState(() {
+                                          _isFavoriteMemo = !memoController.memoList[index].isFavoriteMemo!;
+                                        });
+                                        memoController.updateCtr(
+                                          index: memoController.memoList.indexOf(currentContact),
+                                          createdAt: currentContact.createdAt,
+                                          title: currentContact.title,
+                                          mainText: currentContact.mainText,
+                                          selectedCategory: currentContact.selectedCategory,
+                                          isFavoriteMemo: !currentContact.isFavoriteMemo!,
+                                          isCheckedTodo: currentContact.isCheckedTodo!,
+                                        );
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
