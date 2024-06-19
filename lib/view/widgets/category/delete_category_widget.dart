e98ad5b3020bbf1c45ce3e1b98841b5537cfc61e
MinJaehyun <krism01@naver.com>
2024-06-19 17:37:24 +0900

    코드 개선

--- lib/view/widgets/category/delete_category_widget.dart
+++ lib/view/widgets/category/delete_category_widget.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/controller/category_controller.dart';
-import 'package:simple_note/repository/local_data_source/category_repository.dart';
-import 'package:simple_note/repository/local_data_source/memo_repository.dart';
-import 'package:simple_note/model/category.dart';
+import 'package:simple_note/controller/memo_controller.dart';
 import 'package:simple_note/model/memo.dart';
 
 class DeleteCategoryWidget extends StatefulWidget {
-  const DeleteCategoryWidget(this.context, this.index,{super.key});
+  const DeleteCategoryWidget(this.context, this.index, {super.key});
 
   final BuildContext context;
   final int index;
@@ -19,14 +16,29 @@ class DeleteCategoryWidget extends StatefulWidget {
 
 class _DeleteCategoryWidgetState extends State<DeleteCategoryWidget> {
   final _categoryController = Get.find<CategoryController>();
+  final _memoController = Get.find<MemoController>();
+  late String? categoryToDelete;
+
+  @override
+  void initState() {
+    super.initState();
+    categoryToDelete = _categoryController.categoryList[widget.index].categories;
+  }
+
+  void _deleteCategoryAndMoveMemos() {
+    if (categoryToDelete == null) return;
+
+    List<MemoModel> memosToUpdate = _memoController.memoList.where((memo) => memo.selectedCategory == categoryToDelete).toList();
+    for (MemoModel memo in memosToUpdate) {
+      int memoIndex = _memoController.memoList.toList().indexOf(memo);
+      _memoController.updateCtr(
+          index: memoIndex, createdAt: memo.createdAt, title: memo.title, mainText: memo.mainText, selectedCategory: '미분류');
+    }
+    _categoryController.deleteCtr(index: widget.index);
+  }
 
   @override
   Widget build(BuildContext context) {
-    // note: 범주 탭할 때, selectedCategory가 적절히 업데이트되도록 하고, 단, 다이얼로그 내에서는 직접적으로 특정 범주를 참조 해야한다!
-    // note: 아래 categories는 모델의 속성
-    final categoryToDelete = Hive.box<CategoryModel>(CategoryBox).getAt(widget.index)?.categories;
-    // print(categoryToDelete); // 범주에서 선택한 카테고리 출력
-
     return AlertDialog(
       title: const Text("범주를 삭제 하시겠습니까?"),
       content: const Text("이 범주의 모든 내용은 '미분류'로 이동 됩니다."),
@@ -37,17 +49,7 @@ class _DeleteCategoryWidgetState extends State<DeleteCategoryWidget> {
           ),
           child: const Text('삭제'),
           onPressed: () {
-            if (categoryToDelete != null) {
-              Box<MemoModel> box = Hive.box<MemoModel>(MemoBox);
-              List<MemoModel> memosToUpdate = box.values.where((memo) => memo.selectedCategory == categoryToDelete).toList();
-              // 각 메모의 범주를 '미분류'로 업데이트?
-              for (MemoModel memo in memosToUpdate) {
-                int memoIndex = box.values.toList().indexOf(memo);
-                box.putAt(memoIndex, MemoModel(createdAt: memo.createdAt, title: memo.title, mainText: memo.mainText, selectedCategory: '미분류'));
-              }
-              // 카테고리 삭제
-              _categoryController.deleteCtr(index: widget.index);
-            }
+            _deleteCategoryAndMoveMemos();
             Navigator.of(context).pop();
           },
         ),
