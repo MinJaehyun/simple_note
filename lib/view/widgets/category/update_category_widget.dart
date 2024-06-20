637d9a4cb46904e582044ca6713bfdeea55fa798
MinJaehyun <krism01@naver.com>
2024-06-20 21:18:02 +0900

    범주 변경 시, 중복 범주로 변경하지 못하게 설정함

--- lib/view/widgets/category/update_category_widget.dart
+++ lib/view/widgets/category/update_category_widget.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:simple_note/controller/category_controller.dart';
-import 'package:simple_note/repository/local_data_source/category_repository.dart';
-import 'package:simple_note/repository/local_data_source/memo_repository.dart';
-import 'package:simple_note/model/category.dart';
+import 'package:simple_note/controller/memo_controller.dart';
 import 'package:simple_note/model/memo.dart';
-
 
 // 함수가 호출되고 아래 위젯이 호출되는 구조인 이유는?
 // note: showDialog는 build 메서드 내에서 호출되지 않으므로, 다이얼로그를 표시하는 전용 함수를 만들어 호출해야하기 때문이다.
@@ -21,21 +17,21 @@ class UpdatePopupDialog extends StatefulWidget {
 }
 
 class _UpdatePopupDialogState extends State<UpdatePopupDialog> {
-  String? category;
   final _categoryController = Get.find<CategoryController>();
+  final _memoController = Get.find<MemoController>();
+  late String? category;
+  late final categoryToUpdate;
 
   @override
   void initState() {
     super.initState();
     category = widget.currentContact?.categories.toString();
+    categoryToUpdate = _categoryController.categoryList[widget.index].categories;
+    // print(categoryToUpdate);
   }
 
   @override
   Widget build(BuildContext context) {
-    // note: 범주 탭할 때, selectedCategory가 적절히 업데이트되도록 하고, 단, 다이얼로그 내에서는 직접적으로 특정 범주를 참조 해야한다!
-    // note: 아래 categories는 모델의 속성
-    final categoryToUpdate = Hive.box<CategoryModel>(CategoryBox).getAt(widget.index)?.categories;
-
     return AlertDialog(
       title: const Text('중복된 범주로 변경할 수 없습니다'),
       content: TextFormField(
@@ -51,24 +47,39 @@ class _UpdatePopupDialogState extends State<UpdatePopupDialog> {
       ),
       actions: [
         TextButton(
-          style: TextButton.styleFrom(
-            textStyle: Theme.of(context).textTheme.labelLarge,
-          ),
           child: const Text('저장'),
+          style: TextButton.styleFrom(textStyle: Theme.of(context).textTheme.labelLarge),
           onPressed: () {
-            if (categoryToUpdate != null) {
-              _categoryController.updateCtr(widget.index, category!);
-
-              Box<MemoModel> box = Hive.box<MemoModel>(MemoBox);
-              List<MemoModel> memosToUpdate = box.values.where((memo) => memo.selectedCategory == categoryToUpdate).toList();
-
-              for (MemoModel memo in memosToUpdate) {
-                int memoIndex = box.values.toList().indexOf(memo);
-                box.putAt(
-                    memoIndex, MemoModel(createdAt: memo.createdAt, title: memo.title, mainText: memo.mainText, selectedCategory: category));
+            var isDuplicate = false;
+            for (var categories in _categoryController.categoryList)
+              if (categories.categories! == category) {
+                isDuplicate = true;
+                break;
               }
-            }
+            if (!isDuplicate) {
+              if (categoryToUpdate != null) {
+                _categoryController.updateCtr(widget.index, category!);
+                List<MemoModel> memosToUpdate = _memoController.memoList.where((memo) => memo.selectedCategory == categoryToUpdate).toList();
 
+                for (MemoModel memo in memosToUpdate) {
+                  int memoIndex = _memoController.memoList.indexOf(memo);
+                  _memoController.updateCtr(
+                    index: memoIndex,
+                    createdAt: memo.createdAt,
+                    title: memo.title,
+                    mainText: memo.mainText,
+                    selectedCategory: category,
+                  );
+                }
+              }
+            } else {
+              Get.snackbar(
+                '오류',
+                '이미 존재하는 범주입니다.',
+                snackPosition: SnackPosition.BOTTOM,
+                duration: Duration(seconds: 2),
+              );
+            }
             Navigator.of(context).pop();
           },
         ),
