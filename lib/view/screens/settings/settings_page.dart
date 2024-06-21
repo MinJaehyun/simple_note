2f0fe526a963e857d9c52108b1792abd697252e3
MinJaehyun <krism01@naver.com>
2024-06-21 15:29:38 +0900

    코드 개선

--- lib/view/screens/settings/settings_page.dart
+++ lib/view/screens/settings/settings_page.dart
@@ -15,11 +15,29 @@ class Settings extends StatefulWidget {
 
 class _SettingsState extends State<Settings> {
   final settingsController = Get.find<SettingsController>();
+  late double selectedFont;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedFont = settingsController.fontSizeSlider.value.toDouble();
+  }
+
+  radioListTileFunc({required String title, required SelectedFont value}) {
+    return RadioListTile.adaptive(
+      title: Text(title),
+      value: value,
+      groupValue: settingsController.selectedFont.value,
+      onChanged: (SelectedFont? value) {
+        if (value != null) {
+          settingsController.updateFont(value);
+        }
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    double selectedFont = settingsController.fontSizeSlider.value.toDouble();
-
     return SafeArea(
       child: Scaffold(
         appBar: PreferredSize(
@@ -38,10 +56,7 @@ class _SettingsState extends State<Settings> {
                   children: [
                     const Padding(
                       padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        '사용자 정의',
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue),
-                      ),
+                      child: Text('사용자 정의', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue)),
                     ),
                     Card(
                       child: Obx(
@@ -64,12 +79,9 @@ class _SettingsState extends State<Settings> {
                                   },
                                 ),
                               ),
-
                               // note: 격자 설정
                               ListTile(
-                                leading: settingsController.isGridMode == true
-                                    ? const Icon(Icons.apps)
-                                    : const Icon(Icons.crop_din),
+                                leading: settingsController.isGridMode == true ? const Icon(Icons.apps) : const Icon(Icons.crop_din),
                                 title: const Text('격자 설정'),
                                 trailing: Switch(
                                   value: settingsController.isGridMode == true,
@@ -81,7 +93,6 @@ class _SettingsState extends State<Settings> {
                                   },
                                 ),
                               ),
-
                               // note: 폰트 설정
                               ListTile(
                                 leading: const Icon(Icons.text_format),
@@ -96,93 +107,22 @@ class _SettingsState extends State<Settings> {
                                           Divider(),
                                         ],
                                       ),
-                                      content: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          // note: RadioListTile.adaptive는 플랫폼에 따른(ios, android) 적응형 디자인을 제공하므로 다양한 플랫폼에서 일관된 사용자 경험을 유지하고자 할 때 매우 유용하다
-                                          Obx(
-                                            () => RadioListTile.adaptive(
-                                              title: const Text('기본 폰트'),
-                                              value: SelectedFont.pretendard,
-                                              groupValue: settingsController.selectedFont.value,
-                                              onChanged: (SelectedFont? value) {
-                                                if (value != null) {
-                                                  settingsController.updateFont(value);
-                                                }
-                                              },
-                                            ),
-                                          ),
-                                          Obx(
-                                            () => RadioListTile.adaptive(
-                                              title: const Text('나눔고딕 D2coding'),
-                                              value: SelectedFont.d2coding,
-                                              groupValue: settingsController.selectedFont.value,
-                                              onChanged: (SelectedFont? value) {
-                                                if (value != null) {
-                                                  settingsController.updateFont(value);
-                                                }
-                                              },
-                                            ),
-                                          ),
-                                          Obx(
-                                            () => RadioListTile.adaptive(
-                                              title: const Text('나눔손글씨 붓'),
-                                              value: SelectedFont.nanumBrush,
-                                              groupValue: settingsController.selectedFont.value,
-                                              onChanged: (SelectedFont? value) {
-                                                if (value != null) {
-                                                  settingsController.updateFont(value);
-                                                }
-                                              },
-                                            ),
-                                          ),
-                                          Obx(
-                                            () => RadioListTile.adaptive(
-                                              title: const Text('나눔명조'),
-                                              value: SelectedFont.nanumMyeongjo,
-                                              groupValue: settingsController.selectedFont.value,
-                                              onChanged: (SelectedFont? value) {
-                                                if (value != null) {
-                                                  settingsController.updateFont(value);
-                                                }
-                                              },
-                                            ),
-                                          ),
-                                          Obx(
-                                            () => RadioListTile.adaptive(
-                                              title: const Text('나눔손글씨 펜'),
-                                              value: SelectedFont.nanumPen,
-                                              groupValue: settingsController.selectedFont.value,
-                                              onChanged: (SelectedFont? value) {
-                                                if (value != null) {
-                                                  settingsController.updateFont(value);
-                                                }
-                                              },
-                                            ),
-                                          ),
-                                          Obx(
-                                            () => RadioListTile.adaptive(
-                                              title: const Text('나눔스퀘어 네오'),
-                                              value: SelectedFont.NanumSquareNeo,
-                                              groupValue: settingsController.selectedFont.value,
-                                              onChanged: (SelectedFont? value) {
-                                                if (value != null) {
-                                                  settingsController.updateFont(value);
-                                                }
-                                              },
-                                            ),
-                                          ),
-                                        ],
+                                      content: Obx(
+                                        () => Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            // note: RadioListTile.adaptive는 플랫폼에 따른(ios, android) 적응형 디자인을 제공하므로 다양한 플랫폼에서 일관된 사용자 경험을 유지하고자 할 때 매우 유용하다
+                                            radioListTileFunc(title: '기본 폰트', value: SelectedFont.pretendard),
+                                            radioListTileFunc(title: '나눔고딕 D2coding', value: SelectedFont.d2coding),
+                                            radioListTileFunc(title: '나눔손글씨 붓', value: SelectedFont.nanumBrush),
+                                            radioListTileFunc(title: '나눔명조', value: SelectedFont.nanumMyeongjo),
+                                            radioListTileFunc(title: '나눔손글씨 펜', value: SelectedFont.nanumPen),
+                                            radioListTileFunc(title: '나눔스퀘어 네오', value: SelectedFont.NanumSquareNeo),
+                                          ],
+                                        ),
                                       ),
                                       actions: [
-                                        TextButton(
-                                          onPressed: Get.back,
-                                          child: const Text('저장'),
-                                        ),
-                                        TextButton(
-                                          onPressed: Get.back,
-                                          child: const Text('취소'),
-                                        )
+                                        TextButton(onPressed: Get.back, child: const Text('닫기')),
                                       ],
                                     ),
                                   );
@@ -228,10 +168,7 @@ class _SettingsState extends State<Settings> {
                   children: [
                     const Padding(
                       padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        '일반',
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue),
-                      ),
+                      child: Text('일반', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue)),
                     ),
                     Card(
                       child: Column(
@@ -246,16 +183,13 @@ class _SettingsState extends State<Settings> {
                     ),
                   ],
                 ),
-                // 정보: 의견 보내기 / 앱 정보
+                // note: 정보: 의견 보내기 / 앱 정보
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Padding(
                       padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        '정보',
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue),
-                      ),
+                      child: Text('정보', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue)),
                     ),
                     Card(
                       child: Column(
@@ -263,10 +197,9 @@ class _SettingsState extends State<Settings> {
                           ListTile(
                             leading: const Icon(Icons.calendar_today_outlined),
                             title: const Text('개발 로드맵'),
-                            onTap: () {
-                              Get.to(const TimelineStatusPage());
-                            },
+                            onTap: () => Get.to(const TimelineStatusPage()),
                           ),
+                          // note: 메일 보내기 기능 widget으로 분리
                           const SendMail(),
                           const ListTile(
                             leading: Icon(Icons.info),
