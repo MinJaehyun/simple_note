1caecd8c8f1c671bded2e26576833bce20705fab
MinJaehyun <krism01@naver.com>
2024-06-21 15:47:51 +0900

    코드 개선

--- lib/view/screens/settings/timeline_status_page.dart
+++ lib/view/screens/settings/timeline_status_page.dart
@@ -17,23 +17,34 @@ class TimelineStatusPage extends StatelessWidget {
             const SizedBox(
               height: 30,
               width: double.infinity,
-              // color: Colors.grey[300],
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   SizedBox(
                     child: Row(
-                      children: [Icon(Icons.check_circle, color: Colors.green, size: 16), Text('완료')],
+                      children: [
+                        Icon(Icons.check_circle, color: Colors.green, size: 16),
+                        SizedBox(width: 5),
+                        Text('완료'),
+                      ],
                     ),
                   ),
                   SizedBox(
                     child: Row(
-                      children: [Icon(Icons.circle, color: Colors.blue, size: 16), Text('진행 중')],
+                      children: [
+                        Icon(Icons.sync, color: Colors.blue, size: 16),
+                        SizedBox(width: 5),
+                        Text('진행 중'),
+                      ],
                     ),
                   ),
                   SizedBox(
                     child: Row(
-                      children: [Icon(Icons.circle_outlined, color: Colors.grey, size: 16), Text('진행 예정')],
+                      children: [
+                        Icon(Icons.circle_outlined, color: Colors.grey, size: 16),
+                        SizedBox(width: 5),
+                        Text('진행 예정'),
+                      ],
                     ),
                   ),
                 ],
@@ -49,14 +60,15 @@ class TimelineStatusPage extends StatelessWidget {
   }
 }
 
+// todo: 변경된 내용 직접 설정하기
 class _Timeline extends StatelessWidget {
   final List<String> _timelineTexts = [
+    '메모장 이미지 등록 기능 구현',
     '백업 및 복원 (google drive)',
-    '메모장 이미지 등록 기능',
+    '태그',
+    '메모 알람 및 달력 타임 라인',
+    '메모장 스킨',
     '매년 6월 전체 패키지(pubspec.yaml) 업데이트',
-    // '메모장 스킨',
-    // '태그',
-    // '메모 알람 및 달력 타임 라인',
   ];
 
   @override
@@ -81,7 +93,6 @@ class _Timeline extends StatelessWidget {
               contentsBuilder: (_, index) => _DynamicContents(text: _timelineTexts[index]),
               connectorBuilder: (_, index, __) {
                 if (index == 0) {
-                  // todo: 추후 완료되면 아래에 color: Color(0xff6ad192) 색상 넣기
                   return const SolidLineConnector();
                 } else {
                   return const SolidLineConnector();
@@ -89,6 +100,8 @@ class _Timeline extends StatelessWidget {
               },
               indicatorBuilder: (_, index) {
                 switch (data[index]) {
+                  // todo: 추후, 진행 상태 나타내기
+
                   // case _TimelineStatus.done:
                   //   return DotIndicator(
                   //     color: Color(0xff6ad192),
@@ -145,22 +158,25 @@ class _DynamicContents extends StatelessWidget {
       height: 25.0,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(4.0),
-        color: const Color(0xffe6e7e9),
+        color: Colors.green[100],
       ),
       child: Center(
-        child: Text(text, style: const TextStyle(color: Colors.black)),
+        child: Text(text, style: const TextStyle(color: Colors.black45)),
       ),
     );
   }
 }
 
+// todo: 추후, 직접 todo4 늘려서 적용하기 (완료, 진행, 예정,)
 enum _TimelineStatus {
   done,
   sync,
-  inProgress,
   todo,
+  todo2,
+  todo3,
+  todo4,
 }
 
 extension on _TimelineStatus {
-  bool get isInProgress => this == _TimelineStatus.inProgress;
+  bool get done => this == _TimelineStatus.done;
 }
