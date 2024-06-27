f56738a5961c9fc5b0b5cd74a9d14e2a369fa4ac
MinJaehyun <krism01@naver.com>
2024-06-27 17:30:58 +0900

    프로젝트 버전 업데이트

--- lib/view/screens/settings/settings_page.dart
+++ lib/view/screens/settings/settings_page.dart
@@ -207,7 +207,7 @@ class _SettingsState extends State<Settings> {
                               children: [
                                 Text('버전 정보'),
                                 Spacer(),
-                                Text('1.0.1+7'),
+                                Text('1.0.0'),
                               ],
                             ),
                           ),
