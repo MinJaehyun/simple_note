16781bc92a32a3f77c08c3cb29a0dfc6346fa97d
MinJaehyun <krism01@naver.com>
2024-07-05 16:02:58 +0900

    test: 하단 네비게이션 바 스크린 리더

--- lib/view/widgets/public/footer_navigation_bar_widget.dart
+++ lib/view/widgets/public/footer_navigation_bar_widget.dart
@@ -42,115 +42,104 @@ class _FooterNavigationBarWidgetState extends State<FooterNavigationBarWidget> {
         // note: 캘린더
         Semantics(
           label: '달력',
-          child: ExcludeSemantics(
-            child: IconButton(
-              tooltip: '달력',
-              icon: const Icon(Icons.calendar_month),
-              onPressed: () {
-                setState(() {
-                  setTappedAndIndex(0);
-                });
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) {
-                    return const CalendarPage();
-                  },
-                ));
-              },
-              iconSize: 25,
-              color: currentIndex == 0 ? Colors.cyan : Colors.grey,
-            ),
+          child: IconButton(
+            tooltip: '달력',
+            icon: const Icon(Icons.calendar_month),
+            onPressed: () {
+              setState(() {
+                setTappedAndIndex(0);
+              });
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) {
+                  return const CalendarPage();
+                },
+              ));
+            },
+            iconSize: 25,
+            color: currentIndex == 0 ? Colors.cyan : Colors.grey,
           ),
         ),
         // note: 범주
         Semantics(
           label: '범주',
-          child: ExcludeSemantics(
-            child: IconButton(
-              tooltip: '범주',
-              icon: const Icon(Icons.category),
-              onPressed: () {
-                setState(() {
-                  setTappedAndIndex(1);
-                });
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) {
-                    return const CategoryPage();
-                  },
-                ));
-              },
-              iconSize: 25,
-              color: currentIndex == 1 ? Colors.cyan : Colors.grey,
-            ),
+          child: IconButton(
+            tooltip: '범주',
+            icon: const Icon(Icons.category),
+            onPressed: () {
+              setState(() {
+                setTappedAndIndex(1);
+              });
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) {
+                  return const CategoryPage();
+                },
+              ));
+            },
+            iconSize: 25,
+            color: currentIndex == 1 ? Colors.cyan : Colors.grey,
           ),
         ),
 
         // note: 모든 메모
         Semantics(
           label: '메모장',
-          child: ExcludeSemantics(
-            child: IconButton(
-              tooltip: '메모장',
-              icon: const Icon(Icons.edit_note),
-              onPressed: () {
-                setState(() {
-                  setTappedAndIndex(2);
-                });
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) {
-                    return const MemoPage();
-                  },
-                ));
-              },
-              iconSize: 25,
-              color: currentIndex == 2 ? Colors.cyan : Colors.grey,
-            ),
+          child: IconButton(
+            tooltip: '메모장',
+            icon: const Icon(Icons.edit_note),
+            onPressed: () {
+              setState(() {
+                setTappedAndIndex(2);
+              });
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) {
+                  return const MemoPage();
+                },
+              ));
+            },
+            iconSize: 25,
+            color: currentIndex == 2 ? Colors.cyan : Colors.grey,
           ),
         ),
         // note: 휴지통
         Semantics(
           label: '휴지통',
-          // fix: ExcludeSemantics 설정하여, IconButton 내에 tooltip 설명 제외하기
-          child: ExcludeSemantics(
-            child: IconButton(
-              tooltip: '휴지통',
-              icon: const Icon(Icons.delete),
-              onPressed: () {
-                setState(() {
-                  isTapped = true;
-                  currentIndex = 3;
-                });
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) {
-                    return const TrashCanPage();
-                  },
-                ));
-              },
-              iconSize: 25,
-              color: currentIndex == 3 ? Colors.cyan : Colors.grey,
-            ),
+          child: IconButton(
+            tooltip: '휴지통',
+            icon: const Icon(Icons.delete),
+            onPressed: () {
+              setState(() {
+                isTapped = true;
+                currentIndex = 3;
+              });
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) {
+                  return const TrashCanPage();
+                },
+              ));
+            },
+            iconSize: 25,
+            color: currentIndex == 3 ? Colors.cyan : Colors.grey,
           ),
         ),
         // note: 설정
         Semantics(
           label: '설정',
-          child: ExcludeSemantics(
-            child: IconButton(
-              tooltip: '설정',
-              icon: const Icon(Icons.settings),
-              onPressed: () {
-                setState(() {
-                  isTapped = true;
-                  currentIndex = 4;
-                });
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) {
-                    return const Settings();
-                  },
-                ));
-              },
-              iconSize: 25,
-              color: currentIndex == 4 ? Colors.cyan : Colors.grey,
-            ),
+          child: IconButton(
+            tooltip: '설정',
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              setState(() {
+                isTapped = true;
+                currentIndex = 4;
+              });
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) {
+                  return const Settings();
+                },
+              ));
+            },
+            iconSize: 25,
+            color: currentIndex == 4 ? Colors.cyan : Colors.grey,
           ),
         ),
       ],
