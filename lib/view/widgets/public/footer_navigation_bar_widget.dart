a4775afa9aaf4eea37b22b197fe4181aeee841c2
MinJaehyun <krism01@naver.com>
2024-07-19 12:29:59 +0900

    리펙토링: 155 to 75 라인 개선: footer_navigation_bar_widget

--- lib/view/widgets/public/footer_navigation_bar_widget.dart
+++ lib/view/widgets/public/footer_navigation_bar_widget.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:simple_note/controller/settings_controller.dart';
+import 'package:get/get.dart';
 import 'package:simple_note/view/screens/calendar/calendar_page.dart';
 import 'package:simple_note/view/screens/category/category_page.dart';
 import 'package:simple_note/view/screens/memo/memo_page.dart';
 import 'package:simple_note/view/screens/settings/settings_page.dart';
 import 'package:simple_note/view/screens/trash_can/trash_can_page.dart';
 
+// const pages = [CalendarPage(), CategoryPage(), MemoPage(), TrashCanPage(), Settings()];
+
 class FooterNavigationBarWidget extends StatefulWidget {
-  const FooterNavigationBarWidget(this.index, {super.key, this.sortedTime});
+  const FooterNavigationBarWidget(this.index, {super.key});
 
   final int index;
-  final SortedTime? sortedTime;
 
   @override
   State<FooterNavigationBarWidget> createState() => _FooterNavigationBarWidgetState();
 }
 
 class _FooterNavigationBarWidgetState extends State<FooterNavigationBarWidget> {
-  late int currentIndex = 2;
+  late int currentIndex;
   bool isTapped = true;
 
   @override
@@ -36,118 +37,38 @@ class _FooterNavigationBarWidgetState extends State<FooterNavigationBarWidget> {
     return CurvedNavigationBar(
       backgroundColor: Colors.transparent,
       animationDuration: const Duration(milliseconds: 100),
-      index: currentIndex,
       color: Theme.of(context).colorScheme.onPrimary,
-      items: [
-        // note: 캘린더
-        Semantics(
-          label: '달력',
-          child: IconButton(
-            tooltip: '달력',
-            icon: const Icon(Icons.calendar_month),
-            onPressed: () {
-              setState(() {
-                setTappedAndIndex(0);
-              });
-              Navigator.of(context).pushReplacement(MaterialPageRoute(
-                builder: (context) {
-                  return const CalendarPage();
-                },
-              ));
-            },
-            iconSize: 25,
-            color: currentIndex == 0 ? Colors.cyan : Colors.grey,
-          ),
-        ),
-        // note: 범주
-        Semantics(
-          label: '범주',
-          child: IconButton(
-            tooltip: '범주',
-            icon: const Icon(Icons.category),
-            onPressed: () {
-              setState(() {
-                setTappedAndIndex(1);
-              });
-              Navigator.of(context).pushReplacement(MaterialPageRoute(
-                builder: (context) {
-                  return const CategoryPage();
-                },
-              ));
-            },
-            iconSize: 25,
-            color: currentIndex == 1 ? Colors.cyan : Colors.grey,
-          ),
-        ),
-
-        // note: 모든 메모
-        Semantics(
-          label: '메모장',
-          child: IconButton(
-            tooltip: '메모장',
-            icon: const Icon(Icons.edit_note),
-            onPressed: () {
-              setState(() {
-                setTappedAndIndex(2);
-              });
-              Navigator.of(context).pushReplacement(MaterialPageRoute(
-                builder: (context) {
-                  return const MemoPage();
-                },
-              ));
-            },
-            iconSize: 25,
-            color: currentIndex == 2 ? Colors.cyan : Colors.grey,
-          ),
-        ),
-        // note: 휴지통
-        Semantics(
-          label: '휴지통',
-          child: IconButton(
-            tooltip: '휴지통',
-            icon: const Icon(Icons.delete),
-            onPressed: () {
-              setState(() {
-                isTapped = true;
-                currentIndex = 3;
-              });
-              Navigator.of(context).pushReplacement(MaterialPageRoute(
-                builder: (context) {
-                  return const TrashCanPage();
-                },
-              ));
-            },
-            iconSize: 25,
-            color: currentIndex == 3 ? Colors.cyan : Colors.grey,
-          ),
-        ),
-        // note: 설정
-        Semantics(
-          label: '설정',
-          child: IconButton(
-            tooltip: '설정',
-            icon: const Icon(Icons.settings),
-            onPressed: () {
-              setState(() {
-                isTapped = true;
-                currentIndex = 4;
-              });
-              Navigator.of(context).pushReplacement(MaterialPageRoute(
-                builder: (context) {
-                  return const Settings();
-                },
-              ));
-            },
-            iconSize: 25,
-            color: currentIndex == 4 ? Colors.cyan : Colors.grey,
-          ),
-        ),
-      ],
+      index: currentIndex,
       onTap: (index) {
         setState(() {
           currentIndex = index;
           isTapped = true;
         });
+      },
+      items: [
+        // note: 달력
+        buildIconButton(context, tooltip: "달력", icon: const Icon(Icons.calendar_month), index: 0, route: const CalendarPage()),
+        // note: 범주
+        buildIconButton(context, tooltip: "범주", icon: const Icon(Icons.category), index: 1, route: const CategoryPage()),
+        // note: 모든 메모
+        buildIconButton(context, tooltip: "메모장", icon: const Icon(Icons.edit_note), index: 2, route: MemoPage()),
+        // note: 휴지통
+        buildIconButton(context, tooltip: "휴지통", icon: const Icon(Icons.delete), index: 3, route: const TrashCanPage()),
+        // note: 설정
+        buildIconButton(context, tooltip: "설정", icon: const Icon(Icons.settings), index: 4, route: const Settings()),
+      ],
+    );
+  }
+
+  IconButton buildIconButton(BuildContext context, {required String tooltip, required Widget icon, required int index, required Widget route}) {
+    return IconButton(
+      tooltip: tooltip,
+      icon: icon,
+      color: currentIndex == index ? Colors.cyan : Colors.grey,
+      iconSize: 25,
+      onPressed: () {
+        setState(() => setTappedAndIndex(index));
+        Get.off(route);
       },
     );
   }
