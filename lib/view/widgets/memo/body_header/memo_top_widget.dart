addc9e20c1b971b90b391636c36ad51633612682
MinJaehyun <krism01@naver.com>
2024-07-23 15:58:27 +0900

    메모장 탭 디자인 변경

--- lib/view/widgets/memo/body_header/memo_top_widget.dart
+++ lib/view/widgets/memo/body_header/memo_top_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:simple_note/controller/category_controller.dart';
+import 'package:simple_note/controller/settings_controller.dart';
 
 class MemoTopWidget extends StatefulWidget {
   const MemoTopWidget(this.selectedCategory, this.searchControllerText, this.selectedCategoryFunc, this.searchControllerTextFunc, {super.key});
@@ -17,6 +18,7 @@ class MemoTopWidget extends StatefulWidget {
 class _MemoTopWidgetState extends State<MemoTopWidget> {
   final TextEditingController _textController = TextEditingController();
   final categoryController = Get.find<CategoryController>();
+  final settingsController = Get.find<SettingsController>();
 
   @override
   void initState() {
@@ -85,82 +87,128 @@ class _MemoTopWidgetState extends State<MemoTopWidget> {
               ),
               const SizedBox(height: 15),
               // 상단2: 모든, 미분류, 생성한 범주
-              SizedBox(
-                width: 363,
-                height: 56,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      Card(
-                        shape: RoundedRectangleBorder(
-                          side: const BorderSide(color: Colors.black, width: 0.3),
-                          borderRadius: BorderRadius.circular(4.0),
-                        ),
-                        // note: 선택한 범주가 '모든(null)' 이면 빨강 박스로
-                        color: widget.selectedCategory == null ? Colors.red : null,
-                        child: TextButton(
-                          onPressed: () {
-                            setState(() {
-                              widget.selectedCategoryFunc(null);
-                              widget.searchControllerTextFunc(null);
-                              _textController.clear();
-                            });
-                          },
-                          child: Text(
-                            '모든',
-                            // note: 선택한 범주가 '모든(null)' 이면 ...
-                            style: TextStyle(color: widget.selectedCategory == null ? Colors.white : null),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Container(
+                  width: 363,
+                  height: 56,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: settingsController.isThemeMode.value
+                        ? Colors.grey[200]
+                        : Colors.grey[900],
+                  ),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.2),
+                                spreadRadius: 1,
+                                blurRadius: 5,
+                                offset: Offset(0, 4),
+                              ),
+                            ],
                           ),
-                        ),
-                      ),
-                      Card(
-                        shape: RoundedRectangleBorder(
-                          side: const BorderSide(color: Colors.grey, width: 1.0),
-                          borderRadius: BorderRadius.circular(4.0),
-                        ),
-                        color: widget.selectedCategory == '미분류' ? Colors.red : null,
-                        child: TextButton(
-                          onPressed: () {
-                            setState(() {
-                              widget.selectedCategoryFunc('미분류');
-                              widget.searchControllerTextFunc(null);
-                              _textController.clear();
-                            });
-                          },
-                          child: Text(
-                            '미분류',
-                            style: TextStyle(color: widget.selectedCategory == '미분류' ? Colors.white : null),
-                          ),
-                        ),
-                      ),
-                      // note: [] 내에서 ...box 사용하여 ListView 대신 반복하는 방법: SingleChildScrollView 내에 ListView 사용 시 충돌함!
-                      ...categoryController.categoryList.map(
-                        (categoryContact) {
-                          return Card(
+                          child: Card(
                             shape: RoundedRectangleBorder(
-                              side: const BorderSide(color: Colors.grey, width: 1.0),
+                              side: const BorderSide(color: Colors.grey, width: 0.5),
                               borderRadius: BorderRadius.circular(4.0),
                             ),
-                            color: widget.selectedCategory == categoryContact.categories ? Colors.red : null,
-                            clipBehavior: Clip.antiAlias,
+                            // note: 선택한 범주가 '모든(null)' 이면 빨강 박스로
+                            color: widget.selectedCategory == null ? Colors.green : null,
                             child: TextButton(
                               onPressed: () {
                                 setState(() {
-                                  widget.selectedCategoryFunc(categoryContact.categories);
+                                  widget.selectedCategoryFunc(null);
                                   widget.searchControllerTextFunc(null);
                                   _textController.clear();
                                 });
                               },
                               child: Text(
-                                '${categoryContact.categories}',
-                                style: TextStyle(color: widget.selectedCategory == categoryContact.categories ? Colors.white : null),
+                                '모든',
+                                // note: 선택한 범주가 '모든(null)' 이면 ...
+                                style: TextStyle(color: widget.selectedCategory == null ? Colors.white : Colors.grey),
+
                               ),
                             ),
-                          );
-                        },
-                      ),
-                    ],
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.2),
+                                spreadRadius: 1,
+                                blurRadius: 5,
+                                offset: Offset(0, 4),
+                              ),
+                            ],
+                          ),
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              side: const BorderSide(color: Colors.grey, width: 0.5),
+                              borderRadius: BorderRadius.circular(4.0),
+                            ),
+                            color: widget.selectedCategory == '미분류' ? Colors.green : null,
+                            child: TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  widget.selectedCategoryFunc('미분류');
+                                  widget.searchControllerTextFunc(null);
+                                  _textController.clear();
+                                });
+                              },
+                              child: Text(
+                                '미분류',
+                                style: TextStyle(color: widget.selectedCategory == '미분류' ? Colors.white :  Colors.grey),
+                              ),
+                            ),
+                          ),
+                        ),
+                        // note: [] 내에서 ...box 사용하여 ListView 대신 반복하는 방법: SingleChildScrollView 내에 ListView 사용 시 충돌함!
+                        ...categoryController.categoryList.map(
+                          (categoryContact) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.2),
+                                    spreadRadius: 1,
+                                    blurRadius: 5,
+                                    offset: Offset(0, 4),
+                                  ),
+                                ],
+                              ),
+                              child: Card(
+                                shape: RoundedRectangleBorder(
+                                  side: const BorderSide(color: Colors.grey, width: 0.5),
+                                  borderRadius: BorderRadius.circular(4.0),
+                                ),
+                                color: widget.selectedCategory == categoryContact.categories ? Colors.green : null,
+                                clipBehavior: Clip.antiAlias,
+                                child: TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      widget.selectedCategoryFunc(categoryContact.categories);
+                                      widget.searchControllerTextFunc(null);
+                                      _textController.clear();
+                                    });
+                                  },
+                                  child: Text(
+                                    '${categoryContact.categories}',
+                                    style: TextStyle(color: widget.selectedCategory == categoryContact.categories ? Colors.white :  Colors.grey),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
