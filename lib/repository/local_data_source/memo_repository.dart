c506be013f2da8870b3d220992fee353fa7d0a9a
MinJaehyun <krism01@naver.com>
2024-08-09 13:23:37 +0900

    주석 정리

--- lib/repository/local_data_source/memo_repository.dart
+++ lib/repository/local_data_source/memo_repository.dart
@@ -4,6 +4,7 @@ import 'package:simple_note/model/memo.dart';
 const String MemoBox = 'MEMO_BOX';
 
 class MemoRepository {
+  // note: MemoRepository는 데이터 접근 및 저장만을 담당하도록 설계함.
   static final MemoRepository _singleton = MemoRepository._internal();
 
   factory MemoRepository() {
@@ -14,18 +15,16 @@ class MemoRepository {
 
   Box<MemoModel>? memoBox;
 
-  // note: main에서 즉시 호출함
+  // note: main에서 즉시 호출하고 있다
   Future openBox() async {
     memoBox = await Hive.openBox<MemoModel>(MemoBox);
   }
 
-  // note: CRUD
   // read
   Future<List<MemoModel>> getAllMemoRepo() async {
     return memoBox!.values.toList();
   }
 
-  // note: MemoRepository는 데이터 접근 및 저장만을 담당하도록 설계함.
   // add
   Future<void> addMemoRepo(MemoModel memo) async {
     if (memoBox == null) {
@@ -51,7 +50,7 @@ class MemoRepository {
     await memoBox!.deleteAt(index);
   }
 
-  // close box: 앱 종료 시 closeBox 호출: 미사용
+  // close box: 앱 종료 시 closeBox 호출: 미사용중
   Future closeBox() async {
     await memoBox?.close();
   }
